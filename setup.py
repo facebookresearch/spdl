e1e2738248ff9d349914ef07f576fca318b3a8e3
moto <855818+mthrok@users.noreply.github.com>
2023-12-17 06:13:01 +0800

    Update third party build

--- setup.py
+++ setup.py
@@ -6,15 +6,59 @@ from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 
 ROOT_DIR = os.path.dirname(__file__)
+TP_DIR = os.path.join(ROOT_DIR, "third_party")
 
 ext_modules = [
-    Extension("spdl.lib.libspdl_ffmpeg4", sources=[]),
-    Extension("spdl.lib.libspdl_ffmpeg5", sources=[]),
-    Extension("spdl.lib.libspdl_ffmpeg6", sources=[]),
     Extension("spdl.lib._spdl_ffmpeg4", sources=[]),
     Extension("spdl.lib._spdl_ffmpeg5", sources=[]),
     Extension("spdl.lib._spdl_ffmpeg6", sources=[]),
 ]
+
+
+def _get_cmake_commands(build_dir, install_dir, debug):
+    cfg = "Debug" if debug else "Release"
+    deps_build_dir = os.path.join(build_dir, "folly-deps")
+    cmds = [
+        # fmt: off
+        [
+            "cmake",
+            "-S", TP_DIR,
+            "-B", deps_build_dir,
+            f"-DCMAKE_VERBOSE_MAKEFILE=OFF",
+            f"-DCMAKE_INSTALL_MESSAGE=NEVER",
+            f"-DCMAKE_INSTALL_PREFIX={build_dir}",
+            "-GNinja",
+        ],
+        [
+            "cmake",
+            "--build", deps_build_dir,
+            "--target", "install",
+            "--config", cfg,
+        ],
+        [
+            "cmake",
+            "-S", ROOT_DIR,
+            "-B", build_dir,
+            f"-DCMAKE_VERBOSE_MAKEFILE={'ON' if debug else 'OFF'}",
+            f"-DCMAKE_INSTALL_MESSAGE={'ALWAYS' if debug else 'LAZY'}",
+            f"-DCMAKE_INSTALL_PREFIX={build_dir}",
+            f"-DCMAKE_PREFIX_PATH={build_dir}",
+            "-DCMAKE_FIND_USE_PACKAGE_REGISTRY=false",
+            f"-DPYTHON_EXECUTABLE={sys.executable}",
+            "-DSPDL_BUILD_PYTHON_BINDING=ON",
+            f"-DSPDL_PYTHON_BINDING_INSTALL_PREFIX={install_dir}",
+            "-GNinja",
+        ],
+        [
+            "cmake",
+            "--build", build_dir,
+            "--target", "install",
+            "--config", cfg,
+        ],
+        # fmt: on
+    ]
+    return cmds
+
 
 BUILT_ONCE = False
 
@@ -34,44 +78,13 @@ class CMakeBuild(build_ext):
         BUILT_ONCE = True
 
         extdir = os.path.dirname(self.get_ext_fullpath("foo"))
+        install_dir = os.path.abspath(os.path.join(extdir, "spdl"))
 
-        cfg = "Debug" if self.debug else "Release"
+        cmds = _get_cmake_commands(self.build_temp, install_dir, self.debug)
 
-        # fmt: off
-        config_cmd = [
-            "cmake",
-            "-B", self.build_temp,
-            "-S", ROOT_DIR,
-            f"-DCMAKE_BUILD_TYPE={cfg}",
-            f"-DCMAKE_INSTALL_PREFIX={extdir}/spdl",
-            "-DSPDL_BUILD_SAMPLES:BOOL=OFF",
-            # "-DCMAKE_VERBOSE_MAKEFILE=ON",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
-            "-GNinja",
-        ]
-
-        build_cmd = [
-            "cmake",
-            "--build", self.build_temp,
-            "--target", "install",
-        ]
-        # fmt: on
-
-        print(" ".join(config_cmd), flush=True)
-        subprocess.check_call(config_cmd)
-        print(" ".join(build_cmd), flush=True)
-        subprocess.check_call(build_cmd)
-
-    def get_ext_filename(self, fullname):
-        ext_filename = super().get_ext_filename(fullname)
-        parts = ext_filename.split(".")
-        if "_spdl_ffmpeg" not in ext_filename:
-            # remove ABI
-            del parts[-2]
-            if sys.platform == "darwin":
-                parts[-1] = "dylib"
-        ext_filename = ".".join(parts)
-        return ext_filename
+        for cmd in cmds:
+            print(" ".join(cmd), flush=True)
+            subprocess.check_call(cmd)
 
 
 def main():
