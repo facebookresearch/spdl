6ac00246e42b3d5358f03c31fb74b3886e68780d
moto <855818+mthrok@users.noreply.github.com>
2023-12-19 08:12:20 +0800

    Extract common cmake config

---
+++ cmake/spdl_common_config.cmake
@@ -0,0 +1,36 @@
+###############################################################################
+# Global config
+###############################################################################
+if(NOT CMAKE_CXX_STANDARD)
+  set(CMAKE_CXX_STANDARD 20)
+endif()
+message(STATUS "CMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}")
+
+set(CMAKE_C_VISIBILITY_PRESET hidden)
+set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+
+
+###############################################################################
+# CCache
+###############################################################################
+find_program(CCACHE_PROGRAM ccache)
+if(CCACHE_PROGRAM)
+  message(STATUS "Found ccache")
+  set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+  set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+  set(CMAKE_CUDA_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+else()
+  message(STATUS "Could not find ccache. Consider installing ccache to speed up compilation.")
+endif()
+
+###############################################################################
+# FetchContent config
+###############################################################################
+include(FetchContent)
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
+  cmake_policy(SET CMP0135 NEW)
+endif()
+
+# somehow `cmake_policy(SET .. NEW)` does not work for those...
+set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+set(CMAKE_POLICY_DEFAULT_CMP0063 NEW)
