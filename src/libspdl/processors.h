7c78c5e35130be9898f40ef67089a7ab2f2c4dec
moto <855818+mthrok@users.noreply.github.com>
2024-01-05 14:40:38 +0900

    Refactor processor impl to make it agnostic to media type

--- src/libspdl/processors.h
+++ src/libspdl/processors.h
@@ -6,15 +6,14 @@
 
 namespace spdl {
 
-struct DecodeConfig {
-  std::string src;
-  std::vector<double> timestamps;
-
+struct IOConfig {
   // I/O config
   std::optional<std::string> format = std::nullopt;
   std::optional<OptionDict> format_options = std::nullopt;
   int buffer_size = 8096;
+};
 
+struct DecodeConfig {
   // decoder config
   std::optional<std::string> decoder = std::nullopt;
   std::optional<OptionDict> decoder_options = std::nullopt;
@@ -22,7 +21,10 @@ struct DecodeConfig {
 };
 
 std::vector<Frames> decode_video(
-    DecodeConfig cfg,
-    const std::string& filter_desc);
+    const std::string& src,
+    const std::vector<double>& timestamps,
+    const std::string& filter_desc,
+    IOConfig io_cfg,
+    DecodeConfig decode_cfg);
 
 } // namespace spdl
