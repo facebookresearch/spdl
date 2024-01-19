d7efb9a72b7a44c05ff9f6528020e682076dabe1
moto <855818+mthrok@users.noreply.github.com>
2024-01-19 21:34:55 +0900

    Make coro tasks return pointer to FrameContainer
    So that they can be generated and stored in background as heap objects.

--- src/libspdl/decoding.h
+++ src/libspdl/decoding.h
@@ -6,14 +6,14 @@
 
 namespace spdl {
 
-std::vector<FrameContainer> decode_video(
+std::vector<std::unique_ptr<FrameContainer>> decode_video(
     const std::string& src,
     const std::vector<std::tuple<double, double>>& timestamps,
     const std::string& filter_desc,
     const IOConfig& io_cfg,
     const DecodeConfig& decode_cfg);
 
-std::vector<FrameContainer> decode_audio(
+std::vector<std::unique_ptr<FrameContainer>> decode_audio(
     const std::string& src,
     const std::vector<std::tuple<double, double>>& timestamps,
     const std::string& filter_desc,
