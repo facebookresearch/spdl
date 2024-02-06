ba20764202ba97586445ee2f91b665fdb1026d36
moto <855818+mthrok@users.noreply.github.com>
2024-02-06 15:34:41 -0500

    Hide libfmt dependency from public header

--- src/libspdl/core/logging.h
+++ src/libspdl/core/logging.h
@@ -1,14 +1,28 @@
-#include <fmt/core.h>
+#pragma once
+
 #include <exception>
+#include <string>
+
+namespace spdl::core::detail {
+
+std::string format_err(
+    const std::string& msg,
+    const std::string& file,
+    const int line,
+    const std::string& func);
+
+std::string format_err_internal(
+    const std::string& msg,
+    const std::string& file,
+    const int line,
+    const std::string& func);
+
+} // namespace spdl::core::detail
 
 #define SPDL_FAIL(msg)      \
   throw std::runtime_error( \
-      fmt::format("{} ({}:{} - {})", msg, __FILE__, __LINE__, __func__))
+      spdl::core::detail::format_err(msg, __FILE__, __LINE__, __func__));
 
-#define SPDL_FAIL_INTERNAL(msg)             \
-  throw std::runtime_error(fmt::format(     \
-      "[INTERNAL FAILURE] {} ({}:{} - {})", \
-      msg,                                  \
-      __FILE__,                             \
-      __LINE__,                             \
-      __func__))
+#define SPDL_FAIL_INTERNAL(msg)                                     \
+  throw std::runtime_error(spdl::core::detail::format_err_internal( \
+      msg, __FILE__, __LINE__, __func__));
