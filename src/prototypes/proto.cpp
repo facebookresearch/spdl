dc4eb898b0b4a4a5693b63fa447e64fa8392f470
moto <855818+mthrok@users.noreply.github.com>
2024-02-07 15:00:24 -0500

    Make decoding async

--- src/prototypes/proto.cpp
+++ src/prototypes/proto.cpp
@@ -5,6 +5,7 @@
 #include <libspdl/core/decoding.h>
 #include <libspdl/core/utils.h>
 
+#include <fmt/core.h>
 #include <folly/logging/xlog.h>
 
 using namespace spdl::core;
@@ -15,14 +16,26 @@ int main(int argc, char** argv) {
   std::string src =
       "NASAs_Most_Scientifically_Complex_Space_Observatory_Requires_Precision-MP4_small.mp4";
   std::vector<std::shared_ptr<SourceAdoptor>> adoptors = {
-      std::shared_ptr<BasicAdoptor>(new BasicAdoptor("/home/moto/")),
-      std::shared_ptr<MMapAdoptor>(new MMapAdoptor("/home/moto/"))};
+      std::shared_ptr<BasicAdoptor>(new BasicAdoptor()),
+      std::shared_ptr<MMapAdoptor>(new MMapAdoptor())};
 
   XLOG(INFO) << "Executing the coroutine";
+  std::vector<DecodingResultFuture> futures;
   for (auto& adoptor : adoptors) {
-    std::vector<std::tuple<double, double>> ts{{5., 10.}};
+    for (int i = 0; i < 10; ++i) {
+      std::vector<std::tuple<double, double>> ts{{10 * i, 10 * (i + 1)}};
+      futures.emplace_back(decode_video(src, ts, adoptor, {}, {}, ""));
+    }
+  }
+
+  int i = 0;
+  for (auto& future : futures) {
+    XLOG(INFO) << "Checking " << ++i;
     try {
-      auto frames = decode_video(src, ts, adoptor, {}, {}, "");
+      auto frames = future.get();
+      for (auto& f : frames) {
+        XLOG(INFO) << fmt::format("Decoded: {} frames", f->frames.size());
+      }
     } catch (const std::exception& e) {
       XLOG(ERR) << e.what();
     }
