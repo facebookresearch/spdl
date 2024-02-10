ef5cb914d5dff72b513a6d9752a396dd97a37b4c
moto <855818+mthrok@users.noreply.github.com>
2024-02-10 09:10:09 -0500

    Add plot option to benchmark

--- scripts/benchmark_concurrent_decoding.py
+++ scripts/benchmark_concurrent_decoding.py
@@ -11,16 +11,19 @@ _LG = logging.getLogger(__name__)
 
 def _parse_python_args():
     import argparse
+    from pathlib import Path
 
     parser = argparse.ArgumentParser(
         description=__doc__,
     )
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("-i", "--input-video", help="Input video file.", required=True)
+    parser.add_argument("-o", "--output-dir", default=Path("tmp"), type=Path)
     parser.add_argument("--gpu", type=int, default=-1)
     parser.add_argument("--adoptor", default="BasicAdoptor")
     parser.add_argument("--prefix")
     parser.add_argument("--decoder")
+    parser.add_argument("--plot", action="store_true")
     parser.add_argument("--num-jobs", type=int, default=5)
     parser.add_argument("--num-ts", type=int, default=5)
     parser.add_argument("--decoder-threads", type=int, default=8)
@@ -61,7 +64,7 @@ def _test(
         futures.append(libspdl.decode_video(**cfg))
 
     _LG.info("Checking decoding results")
-    result = []
+    results = []
     for future in futures:
         try:
             frames = future.get()
@@ -70,17 +73,28 @@ def _test(
             continue
         for f in frames:
             buffer = libspdl.to_numpy(f)
-            result.append(buffer)
+            results.append(buffer)
     elapsed = time.monotonic() - t0
     _LG.info(f"{elapsed} [sec]")
+    return results
+
+
+def _plot(frames, output_dir):
+    from PIL import Image
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+    for i, f in enumerate(frames):
+        img = Image.fromarray(f)
+        img.save(str(output_dir / f"{i}.png"))
 
 
 def _main():
     args = _parse_python_args()
     _init(args.debug, args.demuxer_threads, args.decoder_threads)
 
-    with spdl.utils.tracing("benchmark.pftrace"):
-        _test(
+    args.output_dir.mkdir(parents=True, exist_ok=True)
+    with spdl.utils.tracing(str(args.output_dir / "benchmark.pftrace")):
+        frames = _test(
             args.input_video,
             args.decoder,
             args.gpu,
@@ -88,6 +102,9 @@ def _main():
             args.prefix,
             args.num_jobs,
             args.num_ts)
+        if args.plot:
+            _plot(frames[0], args.output_dir / "frames")
+            
 
 
 def _init(debug, demuxer_threads, decoder_threads):
