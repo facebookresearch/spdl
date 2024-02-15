584e14dccf18d532f3e922eac8e52cbaeb5ebb18
moto <855818+mthrok@users.noreply.github.com>
2024-02-15 17:42:47 -0500

    Split the allocation and the construction of Storage

--- src/libspdl/core/storage.cpp
+++ src/libspdl/core/storage.cpp
@@ -9,14 +9,8 @@
 #include <folly/logging/xlog.h>
 
 namespace spdl::core {
-namespace {
-void* _get_buffer(size_t size) {
-  TRACE_EVENT("decoding", "storage::_get_buffer");
-  return operator new(size);
-}
-} // namespace
 
-CPUStorage::CPUStorage(size_t size) : data_(_get_buffer(size)) {}
+CPUStorage::CPUStorage(void* data) : data_(data) {}
 CPUStorage::CPUStorage(CPUStorage&& other) noexcept {
   *this = std::move(other);
 }
@@ -27,6 +21,7 @@ CPUStorage& CPUStorage::operator=(CPUStorage&& other) noexcept {
 }
 CPUStorage::~CPUStorage() {
   if (data_) {
+    TRACE_EVENT("decoding", "operator delete");
     operator delete(data_);
   }
 }
@@ -36,24 +31,24 @@ void* CPUStorage::data() const {
 }
 
 #ifdef SPDL_USE_CUDA
-CUDAStorage::CUDAStorage(size_t size, CUstream stream_) : stream(stream_) {
-  XLOG(DBG9) << fmt::format("Allocating CUDA memory ({} bytes)", size);
-  CHECK_CUDA(
-      cudaMallocAsync(&data_, size, 0), "Failed to allocate CUDA memory");
-  XLOG(DBG9) << fmt::format("Allocation queued {}", data_);
-}
+CUDAStorage::CUDAStorage(void* data, CUstream stream_)
+    : data_(data), stream(stream_) {}
+
 CUDAStorage::CUDAStorage(CUDAStorage&& other) noexcept {
   *this = std::move(other);
 }
+
 CUDAStorage& CUDAStorage::operator=(CUDAStorage&& other) noexcept {
   using std::swap;
   swap(data_, other.data_);
   swap(stream, other.stream);
   return *this;
 }
+
 CUDAStorage::~CUDAStorage() {
   if (data_) {
     XLOG(DBG9) << "Freeing CUDA memory " << data_;
+    TRACE_EVENT("decoding", "cudaFreeAsync");
     CHECK_CUDA(cudaFreeAsync(data_, 0), "Failed to free CUDA memory");
   }
 }
