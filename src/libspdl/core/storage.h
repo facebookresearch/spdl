b0d19213fa63ef00b7c54931d36b93cfbda4a9ac
moto <855818+mthrok@users.noreply.github.com>
2024-02-15 19:10:45 -0500

    Revert "Split the allocation and the construction of Storage"
    This reverts commit 584e14dccf18d532f3e922eac8e52cbaeb5ebb18.

--- src/libspdl/core/storage.h
+++ src/libspdl/core/storage.h
@@ -20,7 +20,7 @@ struct CPUStorage : Storage {
   void* data() const override;
 
   CPUStorage() = default;
-  CPUStorage(void* data);
+  CPUStorage(size_t size);
 
   CPUStorage(const CPUStorage&) = delete;
   CPUStorage& operator=(const CPUStorage&) = delete;
@@ -39,8 +39,7 @@ struct CUDAStorage : Storage {
   void* data() const override;
 
   CUDAStorage() = default;
-  // Will allocate the data
-  CUDAStorage(void* data, CUstream stream);
+  CUDAStorage(size_t size, CUstream stream);
 
   CUDAStorage(const CUDAStorage&) = delete;
   CUDAStorage& operator=(const CUDAStorage&) = delete;
