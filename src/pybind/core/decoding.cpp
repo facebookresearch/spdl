fef878be039c6c39675a75d785ae107cc20170e0
moto <855818+mthrok@users.noreply.github.com>
2024-02-17 13:01:40 -0500

    Add support for image decoding
    Note:
    Image decoding is enabled by just by setting timestamp to
    <0, inf> but this can cause a problem if the input is video.
    
    TODO:
    Make the internal and the treatment of timestamp better.

--- src/pybind/core/decoding.cpp
+++ src/pybind/core/decoding.cpp
@@ -101,6 +101,73 @@ void register_pybind(py::module& m) {
       py::arg("filter_desc") = std::string());
 
   m.def(
+      "decode_image",
+      [](const std::string& src,
+         const std::shared_ptr<SourceAdoptor>& adoptor,
+         const std::optional<std::string>& format,
+         const std::optional<OptionDict>& format_options,
+         int buffer_size,
+         const std::optional<std::string>& decoder,
+         const std::optional<OptionDict>& decoder_options,
+         const int cuda_device_index,
+         const py::object& frame_rate,
+         const std::optional<int>& width,
+         const std::optional<int>& height,
+         const std::optional<std::string>& pix_fmt) {
+        return async_decode(
+            MediaType::Video,
+            src,
+            {{0, HUGE_VAL}},
+            adoptor,
+            {format, format_options, buffer_size},
+            {decoder, decoder_options, cuda_device_index},
+            get_video_filter_description(
+                get_frame_rate(frame_rate), width, height, pix_fmt));
+      },
+      py::arg("src"),
+      py::arg("adoptor") = nullptr,
+      py::arg("format") = py::none(),
+      py::arg("format_options") = py::none(),
+      py::arg("buffer_size") = SPDL_DEFAULT_BUFFER_SIZE,
+      py::arg("decoder") = py::none(),
+      py::arg("decoder_options") = py::none(),
+      py::arg("cuda_device_index") = -1,
+      py::arg("frame_rate") = py::none(),
+      py::arg("width") = py::none(),
+      py::arg("height") = py::none(),
+      py::arg("pix_fmt") = py::none());
+
+  m.def(
+      "decode_image",
+      [](const std::string& src,
+         const std::shared_ptr<SourceAdoptor>& adoptor,
+         const std::optional<std::string>& format,
+         const std::optional<OptionDict>& format_options,
+         int buffer_size,
+         const std::optional<std::string>& decoder,
+         const std::optional<OptionDict>& decoder_options,
+         const int cuda_device_index,
+         const std::string& filter_desc) {
+        return async_decode(
+            MediaType::Video,
+            src,
+            {{0, HUGE_VAL}},
+            adoptor,
+            {format, format_options, buffer_size},
+            {decoder, decoder_options, cuda_device_index},
+            filter_desc);
+      },
+      py::arg("src"),
+      py::arg("adoptor") = nullptr,
+      py::arg("format") = py::none(),
+      py::arg("format_options") = py::none(),
+      py::arg("buffer_size") = SPDL_DEFAULT_BUFFER_SIZE,
+      py::arg("decoder") = py::none(),
+      py::arg("decoder_options") = py::none(),
+      py::arg("cuda_device_index") = -1,
+      py::arg("filter_desc") = std::string());
+
+  m.def(
       "decode_audio",
       [](const std::string& src,
          const std::vector<std::tuple<double, double>>& timestamps,
