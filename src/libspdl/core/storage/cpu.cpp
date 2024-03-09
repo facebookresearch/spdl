a27ea7bfbc741eeb6febcb6efa67b150c9fa9515
moto <855818+mthrok@users.noreply.github.com>
2024-03-09 14:09:58 -0500

    Refactor Storage class
    Similar to 3907af8, separate the CUDAStorage source to dedicated file.

---
+++ src/libspdl/core/storage/cpu.cpp
@@ -0,0 +1,34 @@
+#include <libspdl/core/storage.h>
+
+#include <libspdl/core/detail/tracing.h>
+
+namespace spdl::core {
+////////////////////////////////////////////////////////////////////////////////
+// Storage
+////////////////////////////////////////////////////////////////////////////////
+namespace {
+void* _get_buffer(size_t size) {
+  TRACE_EVENT("decoding", "storage::_get_buffer");
+  return operator new(size);
+}
+} // namespace
+
+CPUStorage::CPUStorage(size_t size) : data_(_get_buffer(size)) {}
+CPUStorage::CPUStorage(CPUStorage&& other) noexcept {
+  *this = std::move(other);
+}
+CPUStorage& CPUStorage::operator=(CPUStorage&& other) noexcept {
+  using std::swap;
+  swap(data_, other.data_);
+  return *this;
+}
+CPUStorage::~CPUStorage() {
+  if (data_) {
+    operator delete(data_);
+  }
+}
+
+void* CPUStorage::data() const {
+  return data_;
+}
+} // namespace spdl::core
--- src/libspdl/core/storage/cuda.cpp
+++ src/libspdl/core/storage/cuda.cpp
@@ -1,47 +1,14 @@
 #include <libspdl/core/storage.h>
 
-#include <libspdl/core/detail/tracing.h>
-#ifdef SPDL_USE_CUDA
 #include <libspdl/core/detail/cuda.h>
-#endif
 
 #include <fmt/core.h>
 #include <folly/logging/xlog.h>
 
 namespace spdl::core {
 ////////////////////////////////////////////////////////////////////////////////
-// Storage
-////////////////////////////////////////////////////////////////////////////////
-namespace {
-void* _get_buffer(size_t size) {
-  TRACE_EVENT("decoding", "storage::_get_buffer");
-  return operator new(size);
-}
-} // namespace
-
-CPUStorage::CPUStorage(size_t size) : data_(_get_buffer(size)) {}
-CPUStorage::CPUStorage(CPUStorage&& other) noexcept {
-  *this = std::move(other);
-}
-CPUStorage& CPUStorage::operator=(CPUStorage&& other) noexcept {
-  using std::swap;
-  swap(data_, other.data_);
-  return *this;
-}
-CPUStorage::~CPUStorage() {
-  if (data_) {
-    operator delete(data_);
-  }
-}
-
-void* CPUStorage::data() const {
-  return data_;
-}
-
-////////////////////////////////////////////////////////////////////////////////
 // CUDAStorage
 ////////////////////////////////////////////////////////////////////////////////
-#ifdef SPDL_USE_CUDA
 CUDAStorage::CUDAStorage(size_t size, CUstream stream_) : stream(stream_) {
   XLOG(DBG9) << fmt::format("Allocating CUDA memory ({} bytes)", size);
   CHECK_CUDA(
@@ -67,6 +34,5 @@ CUDAStorage::~CUDAStorage() {
 void* CUDAStorage::data() const {
   return data_;
 }
-#endif
 
 } // namespace spdl::core
