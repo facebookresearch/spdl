060686b4e404deee43d4108f2e88df5c38c8df16
moto <855818+mthrok@users.noreply.github.com>
2024-03-15 00:48:28 -0400

    Get rid of DecodedFrames and `get_media_type` (#18)
    Originally, following how FFmpeg abstracts away the media type
    difference, `DecodedFrames` was responsible to provide methods to tell
    what the media types the derived classes are.
    
    Now we moved to explicitly typed frames, we do not need them.

--- src/libspdl/core/frames/nvdec.cpp
+++ src/libspdl/core/frames/nvdec.cpp
@@ -8,11 +8,6 @@ template <MediaType media_type>
 NvDecFrames<media_type>::NvDecFrames(uint64_t id_, int format_)
     : id(id_), media_format(format_) {}
 
-template <MediaType media_type>
-enum MediaType NvDecFrames<media_type>::get_media_type() const {
-  return media_type;
-}
-
 template struct NvDecFrames<MediaType::Video>;
 template struct NvDecFrames<MediaType::Image>;
 } // namespace spdl::core
