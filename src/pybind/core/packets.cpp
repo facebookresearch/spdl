f976ab74dbe0606cfd7acaef193d919da4371008
moto <855818+mthrok@users.noreply.github.com>
2024-03-21 20:41:21 -0400

    Add pts property to Video/Image Packets

--- src/pybind/core/packets.cpp
+++ src/pybind/core/packets.cpp
@@ -1,6 +1,7 @@
 #include <libspdl/core/packets.h>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <fmt/format.h>
 
@@ -66,27 +67,48 @@ void register_packets(py::module& m) {
         get_codec_info<MediaType::Audio>(self.get_packets()->codecpar));
   });
 
-  _VideoPacketsWrapper.def("__repr__", [](const VideoPacketsWrapper& self) {
-    return fmt::format(
-        "VideoPackets<id={}, src={}, timestamp=({}, {}), frame_rate=({}/{}), num_packets={}, pixel_format={}, {}>",
-        self.get_packets()->id,
-        self.get_packets()->src,
-        std::get<0>(self.get_packets()->timestamp),
-        std::get<1>(self.get_packets()->timestamp),
-        self.get_packets()->frame_rate.num,
-        self.get_packets()->frame_rate.den,
-        self.get_packets()->num_packets(),
-        self.get_packets()->get_media_format_name(),
-        get_codec_info<MediaType::Video>(self.get_packets()->codecpar));
-  });
+  _VideoPacketsWrapper
+      .def_property_readonly(
+          "pts",
+          [](const VideoPacketsWrapper& self) -> std::vector<double> {
+            std::vector<double> ret;
+            auto& packets = self.get_packets();
+            auto base = packets->time_base;
+            for (auto& packet : packets->get_packets()) {
+              ret.push_back(double(packet->pts) * base.num / base.den);
+            }
+            return ret;
+          })
+      .def("__repr__", [](const VideoPacketsWrapper& self) {
+        return fmt::format(
+            "VideoPackets<id={}, src={}, timestamp=({}, {}), frame_rate=({}/{}), num_packets={}, pixel_format={}, {}>",
+            self.get_packets()->id,
+            self.get_packets()->src,
+            std::get<0>(self.get_packets()->timestamp),
+            std::get<1>(self.get_packets()->timestamp),
+            self.get_packets()->frame_rate.num,
+            self.get_packets()->frame_rate.den,
+            self.get_packets()->num_packets(),
+            self.get_packets()->get_media_format_name(),
+            get_codec_info<MediaType::Video>(self.get_packets()->codecpar));
+      });
 
-  _ImagePacketsWrapper.def("__repr__", [](const ImagePacketsWrapper& self) {
-    return fmt::format(
-        "ImagePackets<id={}, src={}, pixel_format={}, {}>",
-        self.get_packets()->id,
-        self.get_packets()->src,
-        self.get_packets()->get_media_format_name(),
-        get_codec_info<MediaType::Image>(self.get_packets()->codecpar));
-  });
+  _ImagePacketsWrapper
+      .def_property_readonly(
+          "pts",
+          [](const ImagePacketsWrapper& self) {
+            auto& packets = self.get_packets();
+            auto base = packets->time_base;
+            auto pts = packets->get_packets().at(0)->pts;
+            return pts * base.num / base.den;
+          })
+      .def("__repr__", [](const ImagePacketsWrapper& self) {
+        return fmt::format(
+            "ImagePackets<id={}, src={}, pixel_format={}, {}>",
+            self.get_packets()->id,
+            self.get_packets()->src,
+            self.get_packets()->get_media_format_name(),
+            get_codec_info<MediaType::Image>(self.get_packets()->codecpar));
+      });
 }
 } // namespace spdl::core
