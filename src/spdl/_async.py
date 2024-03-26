09ea492512f9ccf5bf5d071e87c155dccd91a136
moto <855818+mthrok@users.noreply.github.com>
2024-03-25 22:45:21 -0400

    Remove the long comment about impl detail
    dev note covers it

--- src/spdl/_async.py
+++ src/spdl/_async.py
@@ -61,61 +61,6 @@ _EXCEPTION_BACKOFF = 1.00
 def _to_async_task(func):
     @functools.wraps(func)
     async def async_wrapper(*args, **kwargs):
-        # Implementation note
-        #
-        # This implementation is essentially simplified version of
-        # `loop.run_in_executor`.
-        # https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.run_in_executor
-        #
-        # `loop.run_in_executor` turns synchronous function into asynchronous
-        # function by running the function in a thread and monitoring the
-        # future object.
-        #
-        # Since our function already runs in threads constructed in C++
-        # realm, we mimic what is done at the outer surface of
-        # `ThreadPoolExecutor.submit()`.
-        #
-        # The relevant implementations are found;
-        #
-        # * `loop.run_in_executor`
-        #   https://github.com/python/cpython/blob/bb66600558cb8d5dd9a56f562bd9531eb1e1685f/Lib/asyncio/base_events.py#L887-L888
-        #
-        # * `ThreadPoolExecutor.submit`
-        #    https://github.com/python/cpython/blob/bb66600558cb8d5dd9a56f562bd9531eb1e1685f/Lib/concurrent/futures/thread.py#L164-L180
-        #
-        # * `_WorkItem.run`
-        #    https://github.com/python/cpython/blob/bb66600558cb8d5dd9a56f562bd9531eb1e1685f/Lib/concurrent/futures/thread.py#L53-L64
-        #
-        # As you can see from `_WorkItem.run`, the given synchronous function is
-        # ran normally, and the result is notified by assigining result/exception
-        # to `concurrent.futures.Future` object.
-        #
-        # In to reflect this in `asyncio`, one needs to wrap the
-        # `concurrent.futures.Future` with `asyncio.futures.Future` and await on
-        # it.
-        #
-        # We just need to perform the result assignment in the coroutine, which is
-        # executed in backgroud thread pool.
-        # For this, we cannot pass `Future` Python object to the C++ code as-is.
-        # (I mean, I tried, but could not make it work).
-        # Because the call back will be performed in a thread disconnected from
-        # Python interpreter, so it causes some synchronization issue around GIL,
-        # and fails with segfault or bus error.
-        #
-        # Interestingly, PyBind11 does the right thing and apply some special
-        # handling to the call back function if it is received as C++ function.
-        #
-        # https://stackoverflow.com/a/72933328/3670924
-        #
-        # For assining exception, since it is not straightforward to manually
-        # translate C++ error to Python exception and assign it to future,
-        # we use `notify_exception` helper function which signals Future
-        # of the failure by assining the prefixed RuntimeError.
-        # Then we raise the underlying exception by poking the
-        # `folly::SemiFuture` class.
-        # This way, the error happened on C++ are raised in the same way as
-        # synchronous functions that PyBind11 will translate it for us.
-        #
         future = concurrent.futures.Future()
         assert future.set_running_or_notify_cancel()
 
