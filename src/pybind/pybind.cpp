768dfc393e468c80294478c80449c1f1debd7914
moto <855818+mthrok@users.noreply.github.com>
2024-04-08 22:02:24 -0400

    Remove the original decoding implemantations
    Now we have the equivalent in concurrent API, which uses the same
    bound functions as async API, we can get rid of this.

--- src/pybind/pybind.cpp
+++ src/pybind/pybind.cpp
@@ -14,7 +14,6 @@ void register_utils(py::module&);
 void register_executor(py::module&);
 void register_frames_and_buffers(py::module&);
 void register_future(py::module&);
-void register_results(py::module&);
 void register_packets(py::module&);
 void register_demuxing(py::module&);
 void register_decoding(py::module&);
@@ -29,7 +28,6 @@ PYBIND11_MODULE(SPDL_FFMPEG_EXT_NAME, m) {
   spdl::core::register_executor(m);
   spdl::core::register_frames_and_buffers(m);
   spdl::core::register_future(m);
-  spdl::core::register_results(m);
   spdl::core::register_packets(m);
   spdl::core::register_demuxing(m);
   spdl::core::register_decoding(m);
