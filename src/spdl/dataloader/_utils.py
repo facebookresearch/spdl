fd3e678fac08a830d9e1f8403fbcec8802b21ddd
moto <855818+mthrok@users.noreply.github.com>
2024-04-12 20:56:09 -0400

    Extract threading/asyncio utilities from prototype

---
+++ src/spdl/dataloader/_utils.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from typing import Optional, Union
+
+
+def _iter_file(path, prefix):
+    with open(path, "r") as f:
+        for line in f:
+            if path := line.strip():
+                if prefix:
+                    path = prefix + path
+                yield path
+
+
+def _iter_sample_every_n(gen, offset=0, every_n=1, max=None):
+    offset = offset % every_n
+
+    num = 0
+    for i, item in enumerate(gen):
+        if i % every_n == offset:
+            yield item
+            num += 1
+
+            if max is not None and num >= max:
+                return
+
+
+def _iter_batch(gen, batch_size):
+    batch = []
+    for item in gen:
+        batch.append(item)
+        if len(batch) >= batch_size:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
+
+def _iter_flist(
+    path: Union[str, Path],
+    *,
+    prefix: Optional[str] = None,
+    batch_size: int = 1,
+    n: int = 0,
+    N: int = 1,
+    max: Optional[int] = None,
+):
+    return _iter_batch(
+        _iter_sample_every_n(_iter_file(path, prefix), n, N, max), batch_size
+    )
