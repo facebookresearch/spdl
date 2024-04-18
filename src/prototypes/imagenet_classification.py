90a4593db705b21aa834e835647ddc6070067124
moto <855818+mthrok@users.noreply.github.com>
2024-04-18 09:35:51 -0400

    Refactor classification prototype

--- src/prototypes/imagenet_classification.py
+++ src/prototypes/imagenet_classification.py
@@ -52,54 +52,76 @@ def _parse_args(args):
     return args
 
 
-# Handrole the transforms so as to support `torch.compile`
+def _expand(vals, batch_size, res):
+    return torch.tensor(vals).view(1, 3, 1, 1).expand(batch_size, 3, res, res).clone()
+
+
+# Handroll the transforms so as to support `torch.compile`
 class Transform(torch.nn.Module):
     def __init__(self, mean, std):
         super().__init__()
-        self.register_buffer("mean", torch.tensor(mean).view(1, 3, 1, 1))
-        self.register_buffer("std", torch.tensor(std).view(1, 3, 1, 1))
+        self.register_buffer("mean", mean)
+        self.register_buffer("std", std)
 
     def forward(self, x):
         x = x.float() / 255.0
         return (x - self.mean) / self.std
 
 
-def _get_model(device, compile, use_bf16):
+class Classification(torch.nn.Module):
+    def forward(self, x, classes):
+        probs = torch.nn.functional.softmax(x, dim=-1)
+        top_prob, top_catid = torch.topk(probs, 5)
+        top1 = (top_catid[:, :1] == classes).sum()
+        top5 = (top_catid == classes).sum()
+        return top1, top5
+
+
+class ModelBundle(torch.nn.Module):
+    def __init__(self, model, transform, classification, device, use_bf16):
+        super().__init__()
+        self.model = model
+        self.transform = transform
+        self.classification = classification
+        self.device = device
+        self.use_bf16 = use_bf16
+
+    def forward(self, x, classes):
+        x = x.permute((0, 3, 1, 2)).to(self.device)
+        x = self.transform(x)
+
+        if self.use_bf16:
+            x = x.to(torch.bfloat16)
+
+        output = self.model(x)
+
+        return self.classification(output, classes)
+
+
+def _get_model(batch_size, device, compile, use_bf16):
     model = timm.create_model("mobilenetv3_large_100", pretrained=True)
     model = model.eval().to(device=device)
     if use_bf16:
         model = model.to(dtype=torch.bfloat16)
-    if compile:
-        model = torch.compile(model, mode="max-autotune")
 
     transform = Transform(
-        mean=[0.4850, 0.4560, 0.4060],
-        std=[0.2290, 0.2240, 0.2250],
+        mean=_expand([0.4850, 0.4560, 0.4060], batch_size, 224),
+        std=_expand([0.2290, 0.2240, 0.2250], batch_size, 224),
     ).to(device)
+
+    classification = Classification().to(device)
+
     if compile:
-        transform = torch.compile(transform, mode="max-autotune")
+        with torch.no_grad():
+            mode = "max-autotune"
+            model = torch.compile(model, mode=mode)
+            transform = torch.compile(transform, mode=mode)
 
-    class ModelBundle(torch.nn.Module):
-        def __init__(self, device, model, transform, use_bf16):
-            super().__init__()
-            self.device = device
-            self.model = model
-            self.transform = transform
-            self.use_bf16 = use_bf16
-
-        def forward(self, x):
-            x = x.permute((0, 3, 1, 2)).to(self.device)
-            x = self.transform(x)
-
-            if self.use_bf16:
-                x = x.to(torch.bfloat16)
-
-            return self.model(x)
-
-    return ModelBundle(device, model, transform, use_bf16)
+    return ModelBundle(model, transform, classification, device, use_bf16)
 
 
 def _run_inference(dataloader, model, device):
+    _LG.info("Running inference.")
     t0 = time.monotonic()
     num_frames, num_correct_top1, num_correct_top5 = 0, 0, 0
     try:
@@ -112,16 +134,16 @@ def _run_inference(dataloader, model, device):
             with torch.profiler.record_function(f"iter_{i}"):
                 batch = spdl.io.to_torch(buffer)
                 classes = torch.tensor(classes, dtype=torch.int64).to(device)
+
+                top1, top5 = model(batch, classes)
+
                 num_frames += batch.shape[0]
-
-                output = model(batch)
-
-                probs = torch.nn.functional.softmax(output, dim=-1)
-                top_prob, top_catid = torch.topk(probs, 5)
-                num_correct_top1 += (top_catid[:, :1] == classes).sum().item()
-                num_correct_top5 += (top_catid == classes).sum().item()
+                num_correct_top1 += top1
+                num_correct_top5 += top5
     finally:
         elapsed = time.monotonic() - t0
+        num_correct_top1 = num_correct_top1.item()
+        num_correct_top5 = num_correct_top5.item()
         fps = num_frames / elapsed
         _LG.info(f"FPS={fps:.2f} ({num_frames}/{elapsed:.2f})")
         acc1 = 0 if num_frames == 0 else num_correct_top1 / num_frames
@@ -185,8 +207,7 @@ def _main(args=None):
     batch_gen = _get_batch_generator(args)
 
     device = torch.device("cuda:0")
-    with torch.no_grad():
-        model = _get_model(device, args.compile, args.use_bf16)
+    model = _get_model(args.batch_size, device, args.compile, args.use_bf16)
 
     with (
         torch.no_grad(),
