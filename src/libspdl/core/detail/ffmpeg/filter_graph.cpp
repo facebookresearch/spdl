6d1126fe3deee14b86e4f0afa1227acf25f97e9c
moto <855818+mthrok@users.noreply.github.com>
2024-04-24 11:04:50 -0400

    Fix PTS propagation error when using filters like fps.
    * Fetch the output time_base from filter context.
    * Add test
    * Refactor filter graph

--- src/libspdl/core/detail/ffmpeg/filter_graph.cpp
+++ src/libspdl/core/detail/ffmpeg/filter_graph.cpp
@@ -4,6 +4,7 @@
 #include "libspdl/core/detail/tracing.h"
 
 #include <fmt/format.h>
+#include <folly/logging/xlog.h>
 
 #include <cmath>
 #include <stdexcept>
@@ -17,12 +18,12 @@ extern "C" {
 
 namespace spdl::core::detail {
 namespace {
-
-AVFilterGraphPtr alloc_filter_graph() {
-  TRACE_EVENT("decoding", "avfilter_graph_alloc");
-  AVFilterGraph* ptr = CHECK_AVALLOCATE(avfilter_graph_alloc());
-  ptr->nb_threads = 1;
-  return AVFilterGraphPtr{ptr};
+// for debug
+std::string describe_graph(AVFilterGraph* graph) {
+  char* desc_ = avfilter_graph_dump(graph, NULL);
+  std::string desc{desc_};
+  av_free(static_cast<void*>(desc_));
+  return desc;
 }
 
 std::string get_buffer_arg(
@@ -30,22 +31,7 @@ std::string get_buffer_arg(
     int height,
     const char* pix_fmt_name,
     AVRational time_base,
-    std::optional<Rational> frame_rate,
     AVRational sample_aspect_ratio) {
-  if (frame_rate) {
-    auto fr = frame_rate.value();
-    return fmt::format(
-        "video_size={}x{}:pix_fmt={}:time_base={}/{}:frame_rate={}/{}:pixel_aspect={}/{}",
-        width,
-        height,
-        pix_fmt_name,
-        time_base.num,
-        time_base.den,
-        fr.num,
-        fr.den,
-        sample_aspect_ratio.num,
-        sample_aspect_ratio.den);
-  }
   return fmt::format(
       "video_size={}x{}:pix_fmt={}:time_base={}/{}:pixel_aspect={}/{}",
       width,
@@ -53,6 +39,26 @@ std::string get_buffer_arg(
       pix_fmt_name,
       time_base.num,
       time_base.den,
+      sample_aspect_ratio.num,
+      sample_aspect_ratio.den);
+}
+
+std::string get_buffer_arg(
+    int width,
+    int height,
+    const char* pix_fmt_name,
+    AVRational time_base,
+    Rational frame_rate,
+    AVRational sample_aspect_ratio) {
+  return fmt::format(
+      "video_size={}x{}:pix_fmt={}:time_base={}/{}:frame_rate={}/{}:pixel_aspect={}/{}",
+      width,
+      height,
+      pix_fmt_name,
+      time_base.num,
+      time_base.den,
+      frame_rate.num,
+      frame_rate.den,
       sample_aspect_ratio.num,
       sample_aspect_ratio.den);
 }
@@ -93,16 +99,22 @@ AVFilterContext* create_filter(
 
 DEF_DPtr(AVFilterInOut, avfilter_inout_free); // this defines AVFilterInOutDPtr
 
-AVFilterInOutDPtr get_io(const char* name, AVFilterContext* filter_ctx) {
+AVFilterInOutDPtr get_io(const char* name) {
   AVFilterInOutDPtr io{CHECK_AVALLOCATE(avfilter_inout_alloc())};
   io->name = av_strdup(name);
-  io->filter_ctx = filter_ctx;
   io->pad_idx = 0;
   io->next = nullptr;
   return io;
 }
 
-AVFilterGraphPtr get_filter(
+AVFilterGraphPtr alloc_filter_graph() {
+  TRACE_EVENT("decoding", "avfilter_graph_alloc");
+  AVFilterGraph* ptr = CHECK_AVALLOCATE(avfilter_graph_alloc());
+  ptr->nb_threads = 1;
+  return AVFilterGraphPtr{ptr};
+}
+
+FilterGraph get_filter(
     const char* desc,
     const AVFilter* src,
     const char* src_arg,
@@ -111,15 +123,14 @@ AVFilterGraphPtr get_filter(
   auto p = filter_graph.get();
 
   // 1. Define the filters at the ends
+  AVFilterInOutDPtr in = get_io("in");
+  AVFilterInOutDPtr out = get_io("out");
+
   // Note
   // AVFilterContext* are attached to the graph and will be freed when the
   // graph is freed. So we don't need to free them here.
-  auto src_ctx = create_filter(p, src, "in", src_arg);
-  auto sink_ctx = create_filter(p, sink, "out");
-
-  // 2. Define the middle
-  AVFilterInOutDPtr in = get_io("in", src_ctx);
-  AVFilterInOutDPtr out = get_io("out", sink_ctx);
+  in->filter_ctx = create_filter(p, src, "in", src_arg);
+  out->filter_ctx = create_filter(p, sink, "out");
   {
     TRACE_EVENT("decoding", "avfilter_graph_parse_ptr");
     CHECK_AVERROR(
@@ -139,24 +150,29 @@ AVFilterGraphPtr get_filter(
   //   XLOG(INFO) << "Filter " << i << ": " << p->filters[i]->name;
   // }
 
-  return filter_graph;
+  XLOG(DBG5) << describe_graph(filter_graph.get());
+
+  return FilterGraph{std::move(filter_graph)};
 }
 
 } // namespace
 
-AVFilterGraphPtr get_audio_filter(
+AVFilterContext* FilterGraph::get_src_ctx() {
+  return graph->filters[0];
+}
+
+AVFilterContext* FilterGraph::get_sink_ctx() {
+  return graph->filters[1];
+}
+
+Rational FilterGraph::get_time_base() const {
+  auto ctx = graph->filters[1]->inputs[0];
+  return Rational{ctx->time_base.num, ctx->time_base.den};
+}
+
+FilterGraph get_audio_filter(
     const std::string& filter_description,
     AVCodecContext* codec_ctx) {
-  static const AVFilter *src, *sink;
-  {
-    TRACE_EVENT("decoding", "avfilter_get_by_name(abuffer)");
-    src = avfilter_get_by_name("abuffer");
-  }
-  {
-    TRACE_EVENT("decoding", "avfilter_get_by_name(abuffersink)");
-    sink = avfilter_get_by_name("abuffersink");
-  }
-
   auto arg = get_abuffer_arg(
       codec_ctx->pkt_timebase,
       codec_ctx->sample_rate,
@@ -167,22 +183,17 @@ AVFilterGraphPtr get_audio_filter(
       codec_ctx->channels
 #endif
   );
-  return get_filter(filter_description.c_str(), src, arg.c_str(), sink);
+  return get_filter(
+      filter_description.c_str(),
+      avfilter_get_by_name("abuffer"),
+      arg.c_str(),
+      avfilter_get_by_name("abuffersink"));
 }
 
-AVFilterGraphPtr get_video_filter(
+FilterGraph get_video_filter(
     const std::string& filter_description,
     AVCodecContext* codec_ctx,
-    std::optional<Rational> frame_rate) {
-  static const AVFilter *src, *sink;
-  {
-    TRACE_EVENT("decoding", "avfilter_get_by_name(buffer)");
-    src = avfilter_get_by_name("buffer");
-  }
-  {
-    TRACE_EVENT("decoding", "avfilter_get_by_name(buffersink)");
-    sink = avfilter_get_by_name("buffersink");
-  }
+    Rational frame_rate) {
   auto arg = get_buffer_arg(
       codec_ctx->width,
       codec_ctx->height,
@@ -190,14 +201,26 @@ AVFilterGraphPtr get_video_filter(
       codec_ctx->pkt_timebase,
       frame_rate,
       codec_ctx->sample_aspect_ratio);
-  return get_filter(filter_description.c_str(), src, arg.c_str(), sink);
+  return get_filter(
+      filter_description.c_str(),
+      avfilter_get_by_name("buffer"),
+      arg.c_str(),
+      avfilter_get_by_name("buffersink"));
 }
 
-std::string describe_graph(AVFilterGraph* graph) {
-  char* desc_ = avfilter_graph_dump(graph, NULL);
-  std::string desc{desc_};
-  av_free(static_cast<void*>(desc_));
-  return desc;
+FilterGraph get_image_filter(
+    const std::string& filter_description,
+    AVCodecContext* codec_ctx) {
+  auto arg = get_buffer_arg(
+      codec_ctx->width,
+      codec_ctx->height,
+      av_get_pix_fmt_name(codec_ctx->pix_fmt),
+      codec_ctx->pkt_timebase,
+      codec_ctx->sample_aspect_ratio);
+  return get_filter(
+      filter_description.c_str(),
+      avfilter_get_by_name("buffer"),
+      arg.c_str(),
+      avfilter_get_by_name("buffersink"));
 }
-
 } // namespace spdl::core::detail
