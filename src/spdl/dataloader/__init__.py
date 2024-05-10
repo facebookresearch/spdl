96bdba28b817ae870945204d8c2fe9f2f271c79e
moto <855818+mthrok@users.noreply.github.com>
2024-05-10 16:01:17 -0400

    Add background task executor

--- src/spdl/dataloader/__init__.py
+++ src/spdl/dataloader/__init__.py
@@ -2,10 +2,12 @@
 
 """
 
+from ._bg_task_executor import BackgroundTaskExecutor
 from ._task_runner import apply_async, apply_concurrent, BackgroundGenerator
 
 __all__ = [
     "BackgroundGenerator",
+    "BackgroundTaskExecutor",
     "apply_async",
     "apply_concurrent",
 ]
