628f06e41931e918d4a78c80b54ef9268ec49fba
moto <855818+mthrok@users.noreply.github.com>
2024-05-12 01:01:10 -0400

    Tweak __init__ file.

--- src/spdl/dataloader/__init__.py
+++ src/spdl/dataloader/__init__.py
@@ -1,6 +1,4 @@
-"""`spdl.dataloader` module implements utilities to run I/O operations efficiently.
-
-"""
+"""Utilities to run I/O operations efficiently."""
 
 from ._bg_task_executor import BackgroundTaskExecutor
 from ._task_runner import apply_async, apply_concurrent, BackgroundGenerator
