ed3fc8c20ffe6d6625e058fef426373a5cb15f1d
moto <855818+mthrok@users.noreply.github.com>
2024-05-12 21:24:03 -0400

    Merge the implementation of vision/batch vision conversion

--- src/libspdl/core/conversion.h
+++ src/libspdl/core/conversion.h
@@ -9,10 +9,16 @@ namespace spdl::core {
 CPUBufferPtr convert_audio_frames(const FFmpegAudioFrames* frames);
 
 template <MediaType media_type>
-CPUBufferPtr convert_frames(const FFmpegFramesPtr<media_type>& frames);
+CPUBufferPtr convert_frames(
+    const std::vector<const FFmpegFrames<media_type>*>& batch);
 
 template <MediaType media_type>
-CPUBufferPtr convert_frames(
-    const std::vector<FFmpegFramesPtr<media_type>>& batch);
+CPUBufferPtr convert_frames(const FFmpegFrames<media_type>* frames) {
+  const std::vector<const FFmpegFrames<media_type>*> batch{frames};
+  // Use the same impl as batch conversion
+  auto ret = convert_frames<media_type>(batch);
+  ret->shape.erase(ret->shape.begin()); // Trim the batch dim
+  return ret;
+}
 
 } // namespace spdl::core
