04b655b489c181f00ee98342c102daa56f4d882f
moto <855818+mthrok@users.noreply.github.com>
2024-05-16 15:46:29 -0400

    Update packaging

--- packaging/build_wheel.sh
+++ packaging/build_wheel.sh
@@ -6,8 +6,9 @@ python_version="${1}"
 
 python="/opt/python/${python_version}/bin/python3"
 
-export PATH="${HOME}/.local/bin/:${PATH}"
+export PATH="/opt/python/${python_version}/bin/:${PATH}"
 
 rm -rf build
+
 "${python}" -m pip install -U cmake ninja
 "${python}" -m build --no-isolation --wheel
