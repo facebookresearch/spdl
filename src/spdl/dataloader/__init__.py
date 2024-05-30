0e5d3d170c13a2fb057e49721b7c6a6dac27eee7
moto <855818+mthrok@users.noreply.github.com>
2024-05-29 23:42:52 -0400

    Rename file

--- src/spdl/dataloader/__init__.py
+++ src/spdl/dataloader/__init__.py
@@ -1,7 +1,7 @@
 """Utilities to run I/O operations efficiently."""
 
+from ._bg_generator import apply_async, BackgroundGenerator
 from ._bg_task_executor import BackgroundTaskExecutor
-from ._task_runner import apply_async, BackgroundGenerator
 
 __all__ = [
     "BackgroundGenerator",
