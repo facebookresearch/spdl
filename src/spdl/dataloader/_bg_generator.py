c7339ac27a6933b19ee9dc806b6b39ae35b97fc0
moto <855818+mthrok@users.noreply.github.com>
2024-05-30 18:05:43 -0400

    Add timeout option to BGGenerator

--- src/spdl/dataloader/_bg_generator.py
+++ src/spdl/dataloader/_bg_generator.py
@@ -1,8 +1,9 @@
 import asyncio
 import logging
+import traceback
 import warnings
-from collections.abc import AsyncIterable, Iterator
-from queue import Queue
+from collections.abc import AsyncIterator, Iterator
+from queue import Empty, Queue
 from threading import Event, Thread
 from typing import Generic, TypeVar
 
@@ -18,23 +19,50 @@ T = TypeVar("T")
 
 
 ################################################################################
-# Impl for AsyncTaskRunner
+# Impl for BackgroundGenerator
 ################################################################################
-def _run_agen(loop, aiterable, sentinel, queue, stopped):
+class _Sentinel:
+    """Sentinel object to indicate the end of iteration."""
+
+    def __init__(self):
+        self.err_msg: str | None = None
+
+
+def _run_agen(
+    loop,
+    aiterable: AsyncIterator,
+    sentinel: _Sentinel,
+    queue: Queue,
+    stopped: Event,
+    timeout: int | float | None,
+):
     async def _generator_loop():
-        try:
-            async for item in aiterable:
+        while True:
+            try:
+                item = await asyncio.wait_for(anext(aiterable), timeout)
+            except StopAsyncIteration:
+                return
+            except asyncio.TimeoutError:
+                _LG.warning("The background generator timed out.")
+                sentinel.err_msg = f"Timed out after {timeout} seconds."
+                return
+            except Exception as err:
+                _LG.error(f"The backgroud generator failed with {err}.")
+                sentinel.err_msg = traceback.print_exception(err)
+                return
+            else:
+                _LG.info(f"Putting {item}")
                 queue.put(item)
 
-                if stopped.is_set():
-                    _LG.debug("Stop requested.")
-                    break
-        finally:
-            queue.put(sentinel)
-            stopped.set()
-        _LG.debug("exiting _generator_loop.")
+            if stopped.is_set():
+                _LG.debug("Stop requested.")
+                break
 
-    loop.run_until_complete(_generator_loop())
+    try:
+        loop.run_until_complete(_generator_loop())
+    finally:
+        queue.put(sentinel)
+        stopped.set()
 
 
 class _thread_manager:
@@ -50,10 +78,13 @@ class _thread_manager:
         self.thread.start()
 
     def __exit__(self, exc_type, exc_value, traceback):
-        # If stopped is not set, the background thread is still running,
-        # but queue is no longer consumed.
-        # The queue might get clogged which can block the background thread and
-        # prevent it from joining. Therefore, we need to flush the queue.
+        # The foreground thread exited, and the queue is no longer consumerd.
+        #
+        # If stopped is not set, the background thread is still running, putting
+        # items in queue.
+        # The queue might get clogged, and it can block the background thread.
+        # In turn, it prevent the bg thread from joining.
+        # Therefore, we need to flush the queue.
         #
         # If stopped is set, the background thread is completed.
         # No more items are generated, and the sentinel value might have been consumed.
@@ -86,6 +117,13 @@ class BackgroundGenerator(Generic[T]):
             generated items from the background thread to the main thread.
             If the queue is full, the background thread will be blocked.
 
+        timeout: The maximum time to wait for the generator to yield an item.
+            If the generator does not yield an item within this time, `TimeoutError`
+            is raised.
+
+            This parameter is intended for breaking unforseen situations where
+            the background generator is stuck for some reasons.
+
         loop: If provided, use this event loop to execute the generator.
             Otherwise, a new event loop will be created. When providing a loop,
             `num_workers` is ignored, so the executor must be configured by
@@ -106,32 +144,52 @@ class BackgroundGenerator(Generic[T]):
 
     def __init__(
         self,
-        iterable: AsyncIterable[T],
+        iterable: AsyncIterator[T],
         *,
         num_workers: int | None = 3,
         queue_size: int = 10,
+        timeout: int | float | None = 300,
         loop: asyncio.AbstractEventLoop | None = None,
     ):
         self.iterable = iterable
         self.queue_size = queue_size
         self.loop = _get_loop(num_workers) if loop is None else loop
+        self.timeout = timeout
 
     def __iter__(self) -> Iterator[T]:
         queue = Queue(maxsize=self.queue_size)
         # Used to indicate the end of the queue.
-        sentinel = object()
+        sentinel = _Sentinel()
         # Used to flag cancellation from outside and to
         # flag the completion from the inside.
         stopped = Event()
 
         thread = Thread(
             target=_run_agen,
-            args=(self.loop, self.iterable, sentinel, queue, stopped),
+            args=(self.loop, self.iterable, sentinel, queue, stopped, self.timeout),
         )
 
         with _thread_manager(thread, stopped, queue, sentinel):
-            while (item := queue.get()) is not sentinel:
-                yield item
+            while True:
+                try:
+                    item = queue.get(timeout=self.timeout)
+                except Empty:
+                    # Foreground thread timeout, meaning the background generator is
+                    # too slow or stuck.
+                    raise TimeoutError(
+                        "The background generator did not yield an item "
+                        f"within {self.timeout} seconds."
+                    ) from None
+                else:
+                    if item is not sentinel:
+                        yield item
+                    elif item.err_msg is not None:
+                        # Something wrong happened in the background thread, including timeout
+                        raise RuntimeError(
+                            f"Error occured in background thread: {item.err_msg}"
+                        )
+                    else:
+                        return
 
 
 class BackgroundTaskProcessor(BackgroundGenerator):
