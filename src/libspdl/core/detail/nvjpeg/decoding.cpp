7b7a213122c82eed7196b2a34df955bfe45f2459
moto <855818+mthrok@users.noreply.github.com>
2024-06-03 16:52:51 -0400

    Refactor nvjpeg

--- src/libspdl/core/detail/nvjpeg/decoding.cpp
+++ src/libspdl/core/detail/nvjpeg/decoding.cpp
@@ -15,20 +15,19 @@
 namespace spdl::core::detail {
 namespace {
 
-std::tuple<std::vector<size_t>, bool>
-get_shape(nvjpegOutputFormat_t out_fmt, size_t height, size_t width) {
+std::tuple<size_t, bool> get_shape(nvjpegOutputFormat_t out_fmt) {
   switch (out_fmt) {
     // TODO: Support NVJPEG_OUTPUT_YUV?
     case NVJPEG_OUTPUT_RGB:
       [[fallthrough]];
     case NVJPEG_OUTPUT_BGR:
-      return {{3, height, width}, false};
+      return {3, false};
     case NVJPEG_OUTPUT_RGBI:
       [[fallthrough]];
     case NVJPEG_OUTPUT_BGRI:
-      return {{height, width, 3}, true};
+      return {3, true};
     case NVJPEG_OUTPUT_Y:
-      return {{1, height, width}, false};
+      return {1, false};
     default:
       // It should be already handled by `get_nvjpeg_output_format`
       SPDL_FAIL_INTERNAL(
@@ -36,30 +35,44 @@ get_shape(nvjpegOutputFormat_t out_fmt, size_t height, size_t width) {
   }
 }
 
-std::tuple<CUDABufferPtr, nvjpegImage_t> get_output(
+struct SizeMeta {
+  size_t width;
+  size_t height;
+  size_t num_channels;
+  bool interleaved;
+};
+
+std::tuple<CUDABufferPtr, SizeMeta> get_output(
     nvjpegOutputFormat_t out_fmt,
     size_t height,
     size_t width,
     const CUDAConfig& cuda_config) {
-  auto [shape, interleaved] = get_shape(out_fmt, height, width);
+  auto [num_channels, interleaved] = get_shape(out_fmt);
 
-  auto buffer = cuda_buffer(shape, cuda_config);
+  auto buffer = interleaved
+      ? cuda_buffer({height, width, num_channels}, cuda_config)
+      : cuda_buffer({num_channels, height, width}, cuda_config);
 
-  auto ptr = static_cast<uint8_t*>(buffer->data());
-
-  nvjpegImage_t output;
-  auto num_channels = interleaved ? shape[2] : shape[0];
-  auto pitch = interleaved ? width * num_channels : width;
-  for (int c = 0; c < num_channels; c++) {
-    output.channel[c] = ptr;
-    output.pitch[c] = pitch;
-    ptr += pitch * height;
-  }
-
-  return {std::move(buffer), output};
+  return {
+      std::move(buffer),
+      SizeMeta{
+          .width = width,
+          .height = height,
+          .num_channels = num_channels,
+          .interleaved = interleaved}};
 }
 
-std::tuple<CUDABufferPtr, nvjpegImage_t, int, int> decode(
+void wrap_buffer(CUDABufferPtr& buffer, SizeMeta meta, nvjpegImage_t& image) {
+  auto ptr = static_cast<uint8_t*>(buffer->data());
+  auto pitch = meta.interleaved ? meta.width * meta.num_channels : meta.width;
+  for (int c = 0; c < meta.num_channels; c++) {
+    image.channel[c] = ptr;
+    image.pitch[c] = pitch;
+    ptr += pitch * meta.height;
+  }
+}
+
+std::tuple<CUDABufferPtr, SizeMeta, nvjpegImage_t> decode(
     std::string_view data,
     nvjpegOutputFormat_t fmt,
     const CUDAConfig& cuda_config) {
@@ -89,7 +102,9 @@ std::tuple<CUDABufferPtr, nvjpegImage_t, int, int> decode(
         "Failed to fetch image information.");
   }
 
-  auto [buffer, image] = get_output(fmt, heights[0], widths[0], cuda_config);
+  auto [buffer, meta] = get_output(fmt, heights[0], widths[0], cuda_config);
+  nvjpegImage_t image;
+  wrap_buffer(buffer, meta, image);
 
   // Note: backend is not used by NVJPEG API when using nvjpegDecode().
   //
@@ -112,7 +127,7 @@ std::tuple<CUDABufferPtr, nvjpegImage_t, int, int> decode(
             (CUstream_st*)cuda_config.stream),
         "Failed to decode an image.");
   }
-  return {std::move(buffer), image, widths[0], heights[0]};
+  return {std::move(buffer), meta, image};
 }
 
 } // namespace
@@ -123,25 +138,24 @@ CUDABufferPtr decode_image_nvjpeg(
     int scale_width,
     int scale_height,
     const std::string& pix_fmt) {
-  cudaStream_t cuda_stream = 0;
-
-  auto out_fmt = get_nvjpeg_output_format(pix_fmt);
+  auto fmt = get_nvjpeg_output_format(pix_fmt);
 
   ensure_cuda_initialized();
   set_cuda_primary_context(cuda_config.device_index);
 
-  auto [buffer, decoded, src_width, src_height] =
-      decode(data, out_fmt, cuda_config);
+  auto [buffer, src_meta, decoded] = decode(data, fmt, cuda_config);
 
   if (scale_width > 0 && scale_height > 0) {
-    auto [buffer2, resized] =
-        get_output(out_fmt, scale_height, scale_width, cuda_config);
+    auto [buffer2, meta2] =
+        get_output(fmt, scale_height, scale_width, cuda_config);
+    nvjpegImage_t resized;
+    wrap_buffer(buffer2, meta2, resized);
 
     resize_npp(
-        out_fmt,
+        fmt,
         decoded,
-        src_width,
-        src_height,
+        src_meta.width,
+        src_meta.height,
         resized,
         scale_width,
         scale_height);
