0e3828395f20484ecef85b0ef0b4922422655166
moto <855818+mthrok@users.noreply.github.com>
2024-06-03 06:30:24 -0400

    Add PoC for tiktoken integration (#65)
    This commit adds PoC for integrating tiktoken into SPDL.
    
    tiktoken releases GIL when perfoming tokenization, so naturally it can
    ran in multithread/async io context.
    
    Unfortunately, tiktoken tokenizer returns `list[int]`, which needs to be
    converted to continuous memory, and this process needs to hold GIL, so
    there is a contention. In IMDB testing, this conversion took few micro
    seconds.
    
    Perhaps this can be worked around by calling underlying API, but I don't
    know if that's possible with Rust without modifying the original source
    code.
    
    Other ideas
    - Let NumPy convert the token and transfer the resulting array to
    device. (Not sure if NumPy array can be accessed GIL-free manner.)
    
    Trace
    
    ![Screenshot 2024-06-03 at 12 35
    44 AM](https://github.com/mthrok/spdl/assets/855818/0815c644-e295-42ff-8be6-71066d052f60)
    
    
    [trace_tiktoken.pftrace.zip](https://github.com/user-attachments/files/15528803/trace_tiktoken.pftrace.zip)

---
+++ src/prototypes/tiktoken.py
@@ -0,0 +1,103 @@
+"""Bulk tokenize text files with OpenAI's TikToken"""
+
+import asyncio
+import concurrent.futures
+from pathlib import Path
+
+import spdl.io
+import spdl.utils
+import tiktoken
+import torch
+from spdl.dataset._utils import _iter_flist
+from spdl.io._core import _run_async
+from spdl.lib import _libspdl
+
+
+def _parse_args(args):
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+    )
+    parser.add_argument("--input-flist", type=Path, required=True, help="File lilst")
+    parser.add_argument("--prefix", help="Root directory of the dataset")
+    parser.add_argument("--encoding", default="cl100k_base")
+    parser.add_argument("--num-threads", type=int, default=4)
+    return parser.parse_args(args)
+
+
+def _tokenize(sentence, encoding, cuda_device: int = 0):
+    with spdl.utils.trace_event("encode"):
+        tokens = encoding.encode(sentence)
+    with spdl.utils.trace_event("convert_tokens_1d"):
+        buffer = _libspdl.convert_tokens_1d(tokens)
+    buffer = _libspdl.transfer_to_cuda(
+        buffer,
+        cuda_config=spdl.io.cuda_config(
+            device_index=cuda_device,
+            allocator=(
+                torch.cuda.caching_allocator_alloc,
+                torch.cuda.caching_allocator_delete,
+            ),
+        ),
+    )
+    return buffer
+
+
+async def _bulk_tokenize(file_gen, encoding, queue, sentinel, concurrency=32):
+    semaphore = asyncio.BoundedSemaphore(concurrency)
+
+    async def _func(data):
+        async with semaphore:
+            buffer = await _run_async(_tokenize, data, encoding)
+            await queue.put(buffer)
+
+    tasks = set()
+    for path in file_gen:
+        async with semaphore:
+            data = _read(path)
+            task = asyncio.create_task(_func(data))
+            tasks.add(task)
+            task.add_done_callback(tasks.discard)
+
+    while tasks:
+        await asyncio.sleep(0.1)
+    await queue.put(sentinel)
+
+
+async def _run(file_gen, encoding, concurrency=32):
+    sentinel = object()
+    queue = asyncio.Queue()
+    task = asyncio.create_task(_bulk_tokenize(file_gen, encoding, queue, sentinel))
+
+    while (item := await queue.get()) is not sentinel:
+        print(item)
+
+    await task
+
+
+def _read(path):
+    with open(path, "r") as fp:
+        return fp.read()
+
+
+def _test(input_flist, prefix, encoding, num_threads):
+    encoding = tiktoken.get_encoding(encoding)
+    file_gen = _iter_flist(input_flist, prefix=prefix, max=2000)
+
+    loop = asyncio.new_event_loop()
+    loop.set_default_executor(
+        concurrent.futures.ThreadPoolExecutor(max_workers=num_threads)
+    )
+    with spdl.utils.tracing("trace_tiktoken"):
+        loop.run_until_complete(_run(file_gen, encoding))
+
+
+def _main(args=None):
+    args = _parse_args(args)
+
+    _test(args.input_flist, args.prefix, args.encoding, args.num_threads)
+
+
+if __name__ == "__main__":
+    _main()
