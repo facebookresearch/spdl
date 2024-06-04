9d0bdc2afb02d41b4a2e4ff66637448c1006b22b
moto <855818+mthrok@users.noreply.github.com>
2024-06-04 14:51:34 -0400

    Fix test

--- tests/spdl_unittest/cuda/nvjpeg_decode_test.py
+++ tests/spdl_unittest/cuda/nvjpeg_decode_test.py
@@ -38,42 +38,34 @@ def test_decode_pix_fmt(get_sample):
     assert torch.equal(rgb_tensor[2], bgr_tensor[0])
 
 
-def test_decode_rubbish(get_sample):
+def test_decode_rubbish(get_samples):
     """When decoding fails, it should raise an error instead of segfault then,
     subsequent valid decodings should succeed"""
 
-    cmd = "ffmpeg -hide_banner -y -f lavfi -i testsrc -frames:v 1 sample.jpg"
-    sample = get_sample(cmd, width=320, height=240)
+    cmd = "ffmpeg -hide_banner -y -f lavfi -i testsrc -frames:v 10 sample_%10d.jpg"
+    flist = get_samples(cmd)
 
-    executor = concurrent.futures.ThreadPoolExecutor(1, "SingleDecoderExecutor")
-
-    async def _test(data):
-        for _ in range(10):
-            rubbish = randbytes(2096)
-            with pytest.raises(RuntimeError):
-                await spdl.io.async_decode_image_nvjpeg(
-                    rubbish,
-                    cuda_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
-                    executor=executor,
-                )
-
-        for _ in range(10):
-            buffer = await spdl.io.async_decode_image_nvjpeg(
-                data,
+    for _ in range(10):
+        rubbish = randbytes(2096)
+        with pytest.raises(RuntimeError):
+            spdl.io.decode_image_nvjpeg(
+                rubbish,
                 cuda_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
-                executor=executor,
             )
 
-            tensor = spdl.io.to_torch(buffer)
-            assert tensor.dtype == torch.uint8
-            assert tensor.shape == torch.Size([3, 240, 320])
-            assert tensor.device == torch.device("cuda", DEFAULT_CUDA)
-            assert not torch.equal(tensor[0], tensor[1])
-            assert not torch.equal(tensor[1], tensor[2])
-            assert not torch.equal(tensor[2], tensor[0])
-        return tensor
+    for path in flist:
+        buffer = spdl.io.decode_image_nvjpeg(
+            path,
+            cuda_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
+        )
 
-    asyncio.run(_test(sample.path))
+        tensor = spdl.io.to_torch(buffer)
+        assert tensor.dtype == torch.uint8
+        assert tensor.shape == torch.Size([3, 240, 320])
+        assert tensor.device == torch.device("cuda", DEFAULT_CUDA)
+        assert not torch.equal(tensor[0], tensor[1])
+        assert not torch.equal(tensor[1], tensor[2])
+        assert not torch.equal(tensor[2], tensor[0])
 
 
 def test_decode_resize(get_sample):
