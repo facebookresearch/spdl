8258f39ab94631d2694cbb661152267a9e202ee2
moto <855818+mthrok@users.noreply.github.com>
2024-06-03 21:10:09 -0400

    Split transfer registeration

---
+++ src/python/core/transfer.cpp
@@ -0,0 +1,27 @@
+#include <libspdl/core/conversion.h>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+#include <nanobind/stl/shared_ptr.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/unique_ptr.h>
+
+namespace nb = nanobind;
+
+namespace spdl::core {
+namespace {
+CUDABufferPtr _transfer_buffer(CPUBufferPtr buffer, const CUDAConfig& cfg) {
+  nb::gil_scoped_release g;
+  return transfer_buffer(std::move(buffer), cfg);
+}
+} // namespace
+
+void register_transfer(nb::module_& m) {
+  m.def(
+      "transfer_buffer",
+      &_transfer_buffer,
+      nb::arg("buffer"),
+      nb::kw_only(),
+      nb::arg("cuda_config"));
+}
+} // namespace spdl::core
