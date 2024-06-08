b755ebb7f2824ae0e12407296b6c0ababe1de3d1
moto <855818+mthrok@users.noreply.github.com>
2024-06-07 23:49:17 -0400

    Add PyTorchStyleDataLoader

--- src/spdl/dataloader/__init__.py
+++ src/spdl/dataloader/__init__.py
@@ -7,3 +7,12 @@ __all__ = [
     "BackgroundConsumer",
     "BackgroundGenerator",
 ]
+
+
+def __getattr__(name: str):
+    if name == "PyTorchStyleDataLoader":
+        from . import _torch
+
+        return _torch.PyTorchStyleDataLoader
+
+    raise AttributeError(f"module {__name__} has no attribute {name}")
