d087dd1fd5e6429fa7fcdc6f712716fbef7180c0
moto <855818+mthrok@users.noreply.github.com>
2024-06-12 09:17:07 -0400

    Update PyTorchStyleDataLoader
    To make it closer to the original

--- src/prototypes/torchvision_imagenet_dataset.py
+++ src/prototypes/torchvision_imagenet_dataset.py
@@ -1,4 +1,5 @@
 import asyncio
+import gc
 import time
 from asyncio import Task
 
@@ -6,13 +7,14 @@ import spdl.io
 import spdl.utils
 
 import torch
-from spdl.dataloader import BackgroundGenerator
+from pytorch_spdl.dataloader import DataLoader
 from spdl.io import ImageFrames
+from spdl.lib import _libspdl
 from spdl.utils import run_async
+from spdl.utils._async import _check_exception
 from torch import Tensor
 from torch.profiler import profile
 
-from torch.utils.data import DataLoader, IterableDataset
 from torchvision.datasets.imagenet import ImageNet
 
 root = "/home/moto/local/imagenet"
@@ -29,11 +31,12 @@ def _decode(path: str, width=224, height=224, pix_fmt="rgb24") -> ImageFrames:
 
 
 def _async_decode(path: str) -> Task[ImageFrames]:
-    coro = run_async(_decode, path)
-    return asyncio.create_task(coro)
+    task = asyncio.create_task(run_async(_decode, path))
+    task.add_done_callback(lambda t: _check_exception(t, stacklevel=2))
+    return task
 
 
-async def _collate_fn(samples: list[tuple[str, int]]) -> tuple[Tensor, Tensor]:
+async def _batch_decode(samples: list[tuple[str, int]]) -> tuple[Tensor, Tensor]:
     tasks = [_async_decode(s[0]) for s in samples]
     classes_ = [s[1] for s in samples]
 
@@ -52,73 +55,29 @@ async def _collate_fn(samples: list[tuple[str, int]]) -> tuple[Tensor, Tensor]:
     return spdl.io.to_torch(buffer), torch.tensor(classes_)
 
 
-def _to_aiter(iterable, batch_size, collate_fn, drop_last):
+class _record_gc:
+    def __init__(self):
+        self.record = None
 
-    async def _aiter():
-        batch = []
-        for item in iterable:
-            batch.append(item)
-            if len(batch) == batch_size:
-                yield await collate_fn(batch)
-                batch = []
-        if not drop_last and batch:
-            yield await collate_fn(batch)
-
-    return aiter(_aiter())
+    def __call__(self, phase, info):
+        if phase == "start":
+            # self.record = torch.ops.profiler._record_function_enter_new("gc", None)
+            _libspdl.trace_event_begin("gc")
+        else:
+            # torch.ops.profiler._record_function_exit(self.record)
+            _libspdl.trace_event_end()
 
 
-class _DataSet(IterableDataset):
-    def __init__(
-        self,
-        dataset,
-        collate_fn,
-        batch_size: int,
-        num_workers: int,
-        drop_last: bool = False,
-        prefetch_factor: int = 2,
-        timeout: int | float = 30,
-    ) -> None:
-        self._dataset = dataset
-        self.collate_fn = collate_fn
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-        self.drop_last = drop_last
-        self.prefetch_factor = prefetch_factor
-        self.timeout = timeout
-
-    def __iter__(self):
-        gen = _to_aiter(self._dataset, self.batch_size, self.collate_fn, self.drop_last)
-        bgg = BackgroundGenerator(
-            gen,
-            num_workers=self.num_workers,
-            queue_size=self.prefetch_factor,
-            timeout=self.timeout,
-        )
-        return iter(bgg)
-
-
-def _dataloader(dataset, collate_fn, batch_size, num_workers, prefetch_factor=8):
-    _dataset = _DataSet(
-        dataset,
-        batch_size=batch_size,
-        collate_fn=collate_fn,
-        num_workers=num_workers,
-        prefetch_factor=prefetch_factor,
-    )
-
-    return DataLoader(
-        _dataset,
-        batch_size=None,
-        num_workers=0,
-    )
+# gc.callbacks.append(_record_gc())
+spdl.utils.set_ffmpeg_log_level(0)
 
 
 def _main():
-    dataloader = _dataloader(
+    dataloader = DataLoader(
         ImageNet(root=root, loader=lambda x: x),
-        collate_fn=_collate_fn,
+        collate_fn=_batch_decode,
         batch_size=32,
-        num_workers=8,
+        num_workers=32,
     )
 
     trace_path = "/home/moto/tmp/trace/dataloader"
@@ -130,14 +89,16 @@ def _main():
         num_frames = 0
         try:
             for i, (batch, classes) in enumerate(dataloader):
-                print(batch.shape, batch.dtype, classes.shape, classes.dtype)
+                # print(batch.shape, batch.dtype, classes.shape, classes.dtype)
                 num_frames += batch.shape[0]
 
-                if i == 300:
+                if i == 100:
                     break
+
         finally:
             elapsed = time.monotonic() - t0
-            print(f"{elapsed:.2f} [sec], {num_frames=}")
+            QPS = num_frames / elapsed
+            print(f"{QPS=:.2f}: {elapsed:.2f} [sec], {num_frames=}")
     prof.export_chrome_trace(f"{trace_path}.json")
 
 
--- tests/pytorch_spdl_unittest/dataloader_test.py
+++ tests/pytorch_spdl_unittest/dataloader_test.py
@@ -2,73 +2,13 @@ from collections.abc import Iterator
 from typing import TypeVar
 
 import pytest
-from pytorch_spdl.dataloader import DataLoader
 
-from torch.utils.data import IterableDataset
+import torch
+from pytorch_spdl.dataloader import DataLoader
+from torch.utils.data import DataLoader as PyTorchDataLoader, IterableDataset
 
 
 T = TypeVar("T")
-
-
-async def passthrough(vals: T) -> T:
-    return vals
-
-
-def test_iterable_dataset():
-    """Simple smoke test for IterableDataset."""
-
-    class _DataSet(IterableDataset):
-        def __init__(self, n):
-            super().__init__()
-            self.n = n
-
-        def __iter__(self) -> Iterator[int]:
-            yield from iter(range(self.n))
-
-    dataset = _DataSet(10)
-    dataloader = iter(DataLoader(dataset, batch_size=3, collate_fn=passthrough))
-
-    val = next(dataloader)
-    assert val == [0, 1, 2]
-    val = next(dataloader)
-    assert val == [3, 4, 5]
-    val = next(dataloader)
-    assert val == [6, 7, 8]
-    val = next(dataloader)
-    assert val == [9]
-
-    with pytest.raises(StopIteration):
-        next(dataloader)
-
-
-def test_map_style_dataset():
-    """Simple smoke test for map style dataset."""
-
-    class _DataSet:
-        def __init__(self, n):
-            super().__init__()
-            self.n = n
-
-        def __getitem__(self, i) -> Iterator[int]:
-            return i
-
-        def __len__(self) -> int:
-            return self.n
-
-    dataset = _DataSet(10)
-    dataloader = iter(DataLoader(dataset, batch_size=3, collate_fn=passthrough))
-
-    val = next(dataloader)
-    assert val == [0, 1, 2]
-    val = next(dataloader)
-    assert val == [3, 4, 5]
-    val = next(dataloader)
-    assert val == [6, 7, 8]
-    val = next(dataloader)
-    assert val == [9]
-
-    with pytest.raises(StopIteration):
-        next(dataloader)
 
 
 def test_dataloader_invalid_args():
@@ -79,16 +19,109 @@ def test_dataloader_invalid_args():
         DataLoader(dummy, sampler=dummy, batch_sampler=dummy)
 
     with pytest.raises(ValueError):
-        DataLoader(dummy, sampler=dummy, shuffle=True)
-
-    with pytest.raises(ValueError):
-        DataLoader(dummy, batch_sampler=dummy, shuffle=True)
-
-    with pytest.raises(ValueError):
-        DataLoader(dummy, batch_sampler=dummy, batch_size=2)
-
-    with pytest.raises(ValueError):
         DataLoader(dummy, num_workers=-1)
 
     with pytest.raises(ValueError):
         DataLoader(dummy, prefetch_factor=0)
+
+
+async def passthrough(vals: T) -> T:
+    return vals
+
+
+class _SizedIterable(IterableDataset):
+    def __init__(self, n):
+        super().__init__()
+        self.n = n
+
+    def __iter__(self) -> Iterator[int]:
+        yield from iter(range(self.n))
+
+    def __len__(self) -> int:
+        return self.n
+
+
+def test_iterable_dataset():
+    """Simple smoke test for IterableDataset."""
+
+    dataloader = iter(
+        DataLoader(_SizedIterable(10), batch_size=3, collate_fn=passthrough)
+    )
+
+    val = next(dataloader)
+    assert val == [0, 1, 2]
+    val = next(dataloader)
+    assert val == [3, 4, 5]
+    val = next(dataloader)
+    assert val == [6, 7, 8]
+    val = next(dataloader)
+    assert val == [9]
+
+    with pytest.raises(StopIteration):
+        next(dataloader)
+
+
+def test_iterable_len():
+    ref = PyTorchDataLoader(_SizedIterable(10), batch_size=3)
+    assert len(ref) == 4
+
+    dataloader = DataLoader(_SizedIterable(10), batch_size=3)
+    assert len(dataloader) == 4
+
+
+def test_iterable_reference():
+    ref = iter(PyTorchDataLoader(_SizedIterable(10), batch_size=3))
+
+    dataloader = iter(DataLoader(_SizedIterable(10), batch_size=3))
+    for _ in range(4):
+        v = next(dataloader)
+        ref_v = next(ref)
+        assert torch.all(v == ref_v)
+
+
+class _SizedMap:
+    def __init__(self, n):
+        super().__init__()
+        self.n = n
+
+    def __getitem__(self, i) -> int:
+        return i
+
+    def __len__(self) -> int:
+        return self.n
+
+
+def test_map_style_dataset():
+    """Simple smoke test for map style dataset."""
+    dataset = _SizedMap(10)
+    dataloader = iter(DataLoader(dataset, batch_size=3, collate_fn=passthrough))
+
+    val = next(dataloader)
+    assert val == [0, 1, 2]
+    val = next(dataloader)
+    assert val == [3, 4, 5]
+    val = next(dataloader)
+    assert val == [6, 7, 8]
+    val = next(dataloader)
+    assert val == [9]
+
+    with pytest.raises(StopIteration):
+        next(dataloader)
+
+
+def test_map_len():
+    ref = PyTorchDataLoader(_SizedMap(10), batch_size=3)
+    assert len(ref) == 4
+
+    dataloader = DataLoader(_SizedMap(10), batch_size=3)
+    assert len(dataloader) == 4
+
+
+def test_map_reference():
+    ref = iter(PyTorchDataLoader(_SizedMap(10), batch_size=3))
+
+    dataloader = iter(DataLoader(_SizedMap(10), batch_size=3))
+    for i in range(4):
+        v = next(dataloader)
+        ref_v = next(ref)
+        assert torch.all(v == ref_v)
