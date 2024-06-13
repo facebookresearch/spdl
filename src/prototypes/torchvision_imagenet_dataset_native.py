2d245d63ae677b3aa4dacf2db1cb329c3e46a056
moto <855818+mthrok@users.noreply.github.com>
2024-06-13 09:27:56 -0400

    Add script to test torchvision native dataloading

--- src/prototypes/torchvision_imagenet_dataset_native.py
+++ src/prototypes/torchvision_imagenet_dataset_native.py
@@ -1,19 +1,15 @@
-"""Test the performance of SPDL and PyTorch DataLoader integration"""
+"""Referench script to benchmark the performance of PyTorch DataLoader"""
 
 import contextlib
 import logging
 import time
 from pathlib import Path
 
-import spdl.io
-import spdl.utils
-
 import torch
-from pytorch_spdl.dataloader import DataLoader
-from spdl.io import ImageFrames
 from torch import Tensor
 from torch.profiler import profile
 
+from torchvision.transforms import Compose, Resize, PILToTensor
 from torchvision.datasets.imagenet import ImageNet
 
 
@@ -41,10 +37,6 @@ def _parse_args(args):
         "--prefetch-factor",
         type=int,
         default=2,
-        help=(
-            "Defines how many decoding jobs to run concurrently. "
-            "There will be at most `prefetch_factor` * `num_workers` jobs."
-        ),
     )
     parser.add_argument(
         "--root",
@@ -61,75 +53,23 @@ def _parse_args(args):
         "--max-batches",
         type=int,
     )
-    parser.add_argument(
-        "--use-nvjpeg",
-        action="store_true",
-    )
     args = parser.parse_args(args)
     if args.trace and args.max_batches is None:
         args.max_batches = 300
     return args
 
 
-def _decode(path: str, width=224, height=224, pix_fmt="rgb24") -> ImageFrames:
-    packets = spdl.io.demux_image(path)
-    return spdl.io.decode_packets(
-        packets,
-        filter_desc=spdl.io.get_video_filter_desc(
-            scale_width=224, scale_height=224, pix_fmt="rgb24"
-        ),
-    )
-
-
-def _batch_decode(samples: list[tuple[str, int]]) -> tuple[Tensor, Tensor]:
-    classes = torch.tensor([cls_ for _, cls_ in samples])
-
-    frames = [_decode(path) for path, _ in samples]
-    buffer = spdl.io.convert_frames(frames, pin_memory=True)
-    buffer = spdl.io.transfer_buffer(
-        buffer,
-        cuda_config=spdl.io.cuda_config(
-            device_index=0,
-            allocator=(
-                torch.cuda.caching_allocator_alloc,
-                torch.cuda.caching_allocator_delete,
-            ),
-        ),
-    )
-    return spdl.io.to_torch(buffer), classes
-
-
-def _batch_decode_nvjpeg(samples: list[tuple[str, int]]) -> tuple[Tensor, Tensor]:
-    classes = torch.tensor([cls_ for _, cls_ in samples])
-
-    buffer = spdl.io.load_image_batch_nvjpeg(
-        [path for path, _ in samples],
-        cuda_config=spdl.io.cuda_config(
-            device_index=0,
-            allocator=(
-                torch.cuda.caching_allocator_alloc,
-                torch.cuda.caching_allocator_delete,
-            ),
-        ),
-        width=224,
-        height=224,
-        pix_fmt="rgb",
-        # strict=True,
-    )
-    batch = spdl.io.to_torch(buffer)
-    return batch, classes
-
-
 def _main(args=None):
     args = _parse_args(args)
     _init_logging(args.debug)
 
-    dataloader = DataLoader(
-        ImageNet(root=args.root, loader=lambda x: x),
-        collate_fn=_batch_decode_nvjpeg if args.use_nvjpeg else _batch_decode,
+    dataloader = torch.utils.data.DataLoader(
+        ImageNet(args.root, transform=Compose([Resize((224, 224)), PILToTensor()])),
         batch_size=args.batch_size,
         num_workers=args.num_workers,
         prefetch_factor=args.prefetch_factor,
+        timeout=5,
+        pin_memory=True,
     )
 
     dataloader = iter(dataloader)
@@ -138,14 +78,13 @@ def _main(args=None):
 
     with (
         profile() if args.trace is not None else contextlib.nullcontext() as prof,
-        spdl.utils.tracing(
-            f"{args.trace}.pftrace", buffer_size=4096 * 16, enable=args.trace
-        ),
     ):
         t0 = time.monotonic()
         num_frames = 0
         try:
             for i, (batch, classes) in enumerate(dataloader):
+                batch = batch.to("cuda")
+                classes = classes.to("cuda")
                 # print(batch.shape, batch.dtype, classes.shape, classes.dtype)
                 num_frames += batch.shape[0]
 
@@ -158,7 +97,7 @@ def _main(args=None):
             print(f"{QPS=:.2f}: {elapsed:.2f} [sec], {num_frames=}")
 
     if args.trace:
-        prof.export_chrome_trace(f"{args.trace}.json")
+        prof.export_chrome_trace(f"{args.trace}.native.json")
 
 
 def _init_logging(debug):
