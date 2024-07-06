97f6adec4a5a5f49f4dd50bce1bf591d9758b005
moto <855818+mthrok@users.noreply.github.com>
2024-07-06 18:37:39 -0400

    Support partial and repeated execution of pipeline (#32)

--- tests/spdl_unittest/dataloader/bg_generator_test.py
+++ tests/spdl_unittest/dataloader/bg_generator_test.py
@@ -7,10 +7,12 @@ from spdl.dataloader import AsyncPipeline  # pyre-ignore: [16]
 
 
 async def adouble(i):
+    print(f"adouble({i})")
     return 2 * i
 
 
 async def aplus1(i):
+    print(f"aplus1({i})")
     return 1 + i
 
 
@@ -66,3 +68,38 @@ def test_timeout_not_enough():
     with pytest.raises(TimeoutError):
         for _ in bgg:
             pass
+
+
+def test_run_partial():
+    """bg generator can run pipeline partially and repeatedly"""
+
+    class Generator:
+        def __iter__(self):
+            for i in range(10):
+                print(f"Generating: {i}")
+                yield i
+
+    apl = AsyncPipeline().add_source(iter(Generator())).pipe(adouble).pipe(aplus1)
+
+    bgg = spdl.dataloader.BackgroundGenerator(apl, timeout=None)
+
+    dataloader = iter(bgg.run(3))
+    for i in range(3):
+        assert 2 * i + 1 == next(dataloader)
+
+    with pytest.raises(StopIteration):
+        next(dataloader)
+
+    dataloader = iter(bgg.run(3))
+    for i in range(3, 6):
+        assert 2 * i + 1 == next(dataloader)
+
+    with pytest.raises(StopIteration):
+        next(dataloader)
+
+    dataloader = iter(bgg.run())
+    for i in range(6, 10):
+        assert 2 * i + 1 == next(dataloader)
+
+    with pytest.raises(StopIteration):
+        next(dataloader)
