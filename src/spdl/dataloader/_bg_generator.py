33c7407ef5d32be1b26911053cf440b0ebcf5d24
moto <855818+mthrok@users.noreply.github.com>
2024-07-08 10:17:49 -0400

    Simplify BGG and do not flush the queue (#35)

--- src/spdl/dataloader/_bg_generator.py
+++ src/spdl/dataloader/_bg_generator.py
@@ -1,12 +1,12 @@
 # pyre-unsafe
 
-import asyncio
 import logging
-import traceback
 import warnings
+from asyncio import AbstractEventLoop as EventLoop
 from collections.abc import Iterator
+from contextlib import contextmanager
 from queue import Empty, Queue
-from threading import Event, Thread
+from threading import Thread
 from typing import Generic, TypeVar
 
 from ._pipeline import AsyncPipeline
@@ -21,98 +21,24 @@ __all__ = [
 T = TypeVar("T")
 
 
-################################################################################
-# Impl for BackgroundGenerator
-################################################################################
-class _Sentinel:
-    """Sentinel object to indicate the end of iteration."""
+@contextmanager
+def _run_in_thread(loop: EventLoop, timeout: int | float | None):
+    """Run the given event loop in a thread."""
+    if loop.is_running():
+        raise RuntimeError("Loop must not be running.")
 
-    def __init__(self):
-        self.err_msg: str | None = None
-
-
-def _run_pipeline(
-    loop,
-    coro,
-    sentinel: _Sentinel,
-    queue: Queue,
-    stopped: Event,
-):
-    async def _run():
-        task = asyncio.create_task(coro)
-        tasks = [task]
-
-        while tasks:
-            done, tasks = await asyncio.wait(tasks, timeout=0.1)
-
-            if done:
-                if (err := task.exception()) is not None:
-                    _LG.error(f"The background generator failed with {err}.")
-                    sentinel.err_msg = traceback.print_exception(err)
-                return
-
-            if stopped.is_set():
-                _LG.debug("Stop requested.")
-                task.cancel()
-                return
-
+    thread = Thread(target=loop.run_forever)
+    thread.start()
     try:
-        loop.run_until_complete(_run())
+        yield
     finally:
-        queue.put(sentinel)
-        stopped.set()
-
-
-class _thread_manager:
-    """Context manager to stop and join the background thread."""
-
-    def __init__(
-        self,
-        thread: Thread,
-        stopped: Event,
-        queue: Queue,
-        sentinel: object,
-        timeout: int | float | None,
-    ):
-        self.thread = thread
-        self.stopped = stopped
-        self.queue = queue
-        self.sentinel = sentinel
-        self.timeout = timeout
-
-    def __enter__(self):
-        self.thread.start()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        # The foreground thread exited, and the queue is no longer consumerd.
-        #
-        # If stopped is not set, the background thread is still running, putting
-        # items in queue.
-        # The queue might get clogged, and it can block the background thread.
-        # In turn, it prevent the bg thread from joining.
-        # Therefore, we need to flush the queue.
-        #
-        # If stopped is set, the background thread is completed.
-        # No more items are generated, and the sentinel value might have been consumed.
-        # So we cannot/don't flush the queue.
-        if not self.stopped.is_set():
-            _LG.info("Stopping the background thread.")
-            self.stopped.set()
-            self._flush()
-
-        _LG.info("Waiting for the background thread to join.")
-        self.thread.join(self.timeout)
-        if self.thread.is_alive():
-            _LG.warning(
-                "The background thread did not join after %f seconds.", self.timeout
-            )
+        _LG.info("Stopping event loop and joining the event loop thread.")
+        loop.call_soon_threadsafe(loop.stop)
+        thread.join(timeout)
+        if thread.is_alive():
+            _LG.warning("The background thread did not join after %f seconds.", timeout)
         else:
-            _LG.info("The background thread joined.")
-
-    def _flush(self):
-        _LG.debug("Flushing the queue.")
-        while (_ := self.queue.get()) is not self.sentinel:
-            pass
+            _LG.info("The event loop thread successfully joined.")
 
 
 class BackgroundGenerator(Generic[T]):
@@ -161,7 +87,7 @@ class BackgroundGenerator(Generic[T]):
         num_workers: int | None = 3,
         queue_size: int = 10,
         timeout: int | float | None = 300,
-        loop: asyncio.AbstractEventLoop | None = None,
+        loop: EventLoop | None = None,
     ):
         self.pipeline = pipeline
         self.queue_size = queue_size
@@ -192,55 +118,46 @@ class BackgroundGenerator(Generic[T]):
         )
         return self.run()
 
-    def run(self, num_items: int | None = None) -> Iterator[T]:
+    def run(
+        self, num_items: int | None = None, *, fail_on_error: bool = False
+    ) -> Iterator[T]:
         """Run the generator in background thread and iterate the result.
 
         Args:
             num_items: The number of items to yield. If omitted, the generator
                 will be iterated until the end.
 
+            fail_on_error: When ``True``, raise the exception if pipeline fails. Otherwise,
+                log the error and continue.
+
         Yields:
             Items generated by the provided generator.
         """
-        coro = self.pipeline.run(num_items=num_items)
+        sentinel = object()
 
-        # Used to indicate the end of the queue.
-        sentinel = _Sentinel()
-        # Used to flag cancellation from outside and to
-        # flag the completion from the inside.
-        stopped = Event()
+        async def _run():
+            try:
+                await self.pipeline.run(num_items=num_items)
+            finally:
+                self.queue.put(sentinel)
 
-        thread = Thread(
-            target=_run_pipeline,
-            args=(self.loop, coro, sentinel, self.queue, stopped),
-        )
+        task = self.loop.create_task(_run())
 
-        with _thread_manager(thread, stopped, self.queue, sentinel, self.timeout):
+        with _run_in_thread(self.loop, self.timeout):
             while True:
                 try:
                     item = self.queue.get(timeout=self.timeout)
                 except Empty:
-                    # Foreground thread timeout, meaning the background pipeline is
-                    # too slow or stuck.
+                    # The pipeline is too slow or stuck.
                     raise TimeoutError(
-                        "The background pipeline did not yield an item "
-                        f"within {self.timeout} seconds."
+                        f"The pipeline did not yield an item within {self.timeout} seconds."
                     ) from None
                 else:
-                    # Note:
-                    # In the original implementation, the sentinel value was compared using
-                    # identity checking (`is` operator). But strangely, this stopped working in
-                    # https://github.com/facebookresearch/spdl/pull/32 .
-                    #
-                    # The sentinel object passed to the background thread and came back through
-                    # a queue is no longer the same object. So now we use `isinstance`.
-                    if not isinstance(item, _Sentinel):
-                        yield item
-                    elif item.err_msg is None:
-                        # Sane termination
-                        return
-                    else:
-                        # Something wrong happened in the background thread, including timeout
-                        raise RuntimeError(
-                            f"Error occured in background thread: {item.err_msg}"
-                        )
+                    if item is sentinel:
+                        break
+                    yield item
+
+            if (err := task.exception()) is not None:
+                if fail_on_error:
+                    raise err
+                _LG.exception("Pipeline encountered an error.")
