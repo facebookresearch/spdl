314af1e9c9b7a595f9b7547b9520901ab01ba473
moto-meta <147176069+moto-meta@users.noreply.github.com>
2024-07-27 22:12:19 -0400

    Update license (#119)
    Summary: Update license config to ensure the OSS license
    
    Differential Revision: D60343080
    
    Co-authored-by: Moto Hira <moto@meta.com>

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,4 +1,12 @@
 /**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
  * We add extra br tags to the autodoc output, so each parameter is shown on
  * its own line.
  */
--- src/prototypes/torchvision_imagenet_dataset.py
+++ src/prototypes/torchvision_imagenet_dataset.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 """Test the performance of SPDL and PyTorch DataLoader integration"""
 
 # pyre-ignore-all-errors
