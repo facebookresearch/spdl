c5c4d5ea9370525db45321c15cc359a90c01aeaf
moto <855818+mthrok@users.noreply.github.com>
2025-01-08 23:03:34 -0500

    Add ImageNet source

---
+++ src/spdl/dataloader/_source/_local_directory.py
@@ -0,0 +1,47 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+__all__ = ["LocalDirectory"]
+
+import random
+from collections.abc import Iterator
+from os import PathLike
+from pathlib import Path
+
+from ._type import IterableWithShuffle
+
+# pyre-strict
+
+################################################################################
+# LocalDirectory
+################################################################################
+
+
+def _traverse(root: Path, pattern: str) -> list[Path]:
+    flist = list(root.glob(pattern))
+    flist.sort()
+    return flist
+
+
+class LocalDirectory(IterableWithShuffle[Path]):
+    def __init__(self, root: str | PathLike[str], pattern: str) -> None:
+        """
+
+        .. note::
+
+           See `<https://docs.python.org/3/library/pathlib.html#pattern-language>`_ for the
+           list of supported patterns.
+
+        """
+        self.root = Path(root)
+        self.pattern = pattern
+        self._flist: list[Path] = _traverse(self.root, self.pattern)
+
+    def shuffle(self, seed: int) -> None:
+        random.Random(seed).shuffle(self._flist)
+
+    def __iter__(self) -> Iterator[Path]:
+        yield from self._flist
