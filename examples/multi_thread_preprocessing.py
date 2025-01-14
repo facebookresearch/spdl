315bf698861dadaadb60c8851df749d6d41d23ee
Pyre Bot Jr. <generatedunixname89002005307016@meta.com>
2025-01-14 14:13:40 -0800

    Add missing Pyre mode headers] [batch:21/1522] [shard:38/N]
    Differential Revision: D68144453

--- examples/multi_thread_preprocessing.py
+++ examples/multi_thread_preprocessing.py
@@ -5,6 +5,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 """This example shows how to run PyTorch tarnsform in SPDL Pipeline,
 and compares its performance against PyTorch DataLoader.
 
