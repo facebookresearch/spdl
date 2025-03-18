8a602dc4ca2fb0dc59b24c5aa43afdb0bd33213b
moto <855818+mthrok@users.noreply.github.com>
2025-03-18 10:32:06 -0400

    Fix segfault for built-without-cuda

--- src/spdl/io/lib/cuda/types.cpp
+++ src/spdl/io/lib/cuda/types.cpp
@@ -18,11 +18,22 @@ namespace nb = nanobind;
 
 namespace spdl::cuda {
 void register_types(nb::module_& m) {
-  nb::class_<CUDAConfig>(m, "CUDAConfig")
-      .def(
-          nb::init<int, uintptr_t, std::optional<cuda_allocator>>(),
-          nb::arg("device_index"),
-          nb::arg("stream") = 0,
-          nb::arg("allocator") = nb::none());
+  nb::class_<CUDAConfig>(m, "CUDAConfig");
+
+  m.def(
+      "cuda_config",
+      [](int index,
+         uintptr_t stream,
+         std::optional<cuda_allocator> allocator) -> CUDAConfig {
+#ifdef SPDL_USE_CUDA
+        return CUDAConfig{index, stream, std::move(allocator)};
+#else
+        throw std::runtime_error("SPDL is not built with CUDA support.");
+#endif
+      },
+      nb::call_guard<nb::gil_scoped_release>(),
+      nb::arg("device_index"),
+      nb::arg("stream") = 0,
+      nb::arg("allocator") = nb::none());
 }
 } // namespace spdl::cuda
