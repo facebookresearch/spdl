823a57a63ffda2f41bd38aada73f2ad79d592a1b
moto <855818+mthrok@users.noreply.github.com>
2025-03-18 11:22:54 -0400

    Run NVDEC tests (#500)

--- tests/spdl_unittest/cuda/nvdec_video_decoding_test.py
+++ tests/spdl_unittest/cuda/nvdec_video_decoding_test.py
@@ -30,22 +30,16 @@ def _decode_video(src, timestamp=None, allocator=None, **decode_options):
     return spdl.io.to_torch(buffer)
 
 
-def _decode_videos(src, timestamps):
-    frames = [
-        spdl.io.decode_packets_nvdec(
-            packets,
-            device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
-        )
-        for packets in spdl.io.streaming_demux_video(src, timestamps=timestamps)
-    ]
-
-    return [spdl.io.to_torch(f) for f in frames]
-
-
 @pytest.fixture
 def dummy(get_sample):
     cmd = "ffmpeg -hide_banner -y -f lavfi -i testsrc -frames:v 2 sample.mp4"
     return get_sample(cmd, width=320, height=240)
+
+
+def test_nvdec_no_file():
+    """Does not crash when handling non-existing file"""
+    with pytest.raises(RuntimeError):
+        _decode_video("lcbjbbvbhiibdctiljrnfvttffictefh.mp4")
 
 
 def test_nvdec_odd_size(dummy):
@@ -70,6 +64,27 @@ def test_nvdec_negative(dummy):
 
     with pytest.raises(RuntimeError):
         _decode_video(dummy.path, crop_bottom=-1)
+
+
+def test_nvdec_video_smoke_test(get_sample):
+    """Can decode video with NVDEC"""
+    cmd = "ffmpeg -hide_banner -y -f lavfi -i testsrc,format=yuv420p -frames:v 1000 sample.mp4"
+
+    sample = get_sample(cmd, width=320, height=240)
+
+    packets = spdl.io.demux_video(sample.path)
+    print(packets)
+    buffer = spdl.io.decode_packets_nvdec(
+        packets,
+        device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
+    )
+
+    tensor = spdl.io.to_torch(buffer)
+    print(f"{tensor.shape=}, {tensor.dtype=}, {tensor.device=}")
+    assert tensor.shape[0] == 1000
+    assert tensor.shape[1] == 4
+    assert tensor.shape[2] == 240
+    assert tensor.shape[3] == 320
 
 
 def _save(array, prefix):
@@ -106,7 +121,7 @@ def test_nvdec_decode_h264_420p_basic(h264):
     # _save(v, "./base_v")
 
     assert array.dtype == torch.uint8
-    assert array.shape == (25, 4, h264.height, h264.width)
+    assert array.shape == (24, 4, h264.height, h264.width)
 
 
 # TODO: Test other formats like MJPEG, MPEG, HEVC, VC1 AV1 etc...
@@ -140,7 +155,7 @@ def test_nvdec_decode_video_torch_allocator(h264):
         assert allocator_called
         assert not deleter_called
         assert array.dtype == torch.uint8
-        assert array.shape == (25, 4, h264.height, h264.width)
+        assert array.shape == (24, 4, h264.height, h264.width)
 
     _test()
 
@@ -186,7 +201,7 @@ def test_nvdec_decode_h264_420p_resize(h264):
     # _save(array, "./resize")
 
     assert array.dtype == torch.uint8
-    assert array.shape == (25, 4, height, width)
+    assert array.shape == (24, 4, height, width)
 
 
 def test_nvdec_decode_h264_420p_crop(h264):
@@ -205,7 +220,7 @@ def test_nvdec_decode_h264_420p_crop(h264):
     )
 
     assert rgba.dtype == torch.uint8
-    assert rgba.shape == (25, 4, h, w)
+    assert rgba.shape == (24, 4, h, w)
 
     rgba0 = _decode_video(
         h264.path,
@@ -234,47 +249,18 @@ def test_nvdec_decode_crop_resize(h264):
     )
 
     assert array.dtype == torch.uint8
-    assert array.shape == (25, 4, h, w)
+    assert array.shape == (24, 4, h, w)
 
 
-def test_num_frames_arithmetics(h264):
-    """NVDEC with non-zero start time should produce proper number of frames."""
-    ref = _decode_video(
-        h264.path,
-        timestamp=(0, 1.0),
-    )
-
-    # NOTE: The source video has 25 FPS.
-    # fmt: off
-    cfgs = [
-        # timestamp, args_for_slicing
-        ((0, 0.2), [5]),
-        ((0, 0.4), [10]),
-        ((0, 0.6), [15]),
-        ((0, 0.8), [20]),
-        ((0, 1.0,), [25]),
-        ((0.2, 0.4), [5, 10]),
-        ((0.2, 0.6), [5, 15]),
-        ((0.2, 0.8), [5, 20]),
-        ((0.2, 1.0), [5, 25]),
-        ((0.4, 0.6), [10, 15]),
-        ((0.4, 0.8), [10, 20]),
-        ((0.4, 1.0), [10, 25]),
-        ((0.6, 0.8), [15, 20]),
-        ((0.6, 1.0), [15, 25]),
-        ((0.8, 1.0), [20, 25]),
-    ]
-    # fmt: on
-    frames = _decode_videos(
-        h264.path,
-        timestamps=[cfg[0] for cfg in cfgs],
-    )
-
-    for (ts, slice_args), fs in zip(cfgs, frames, strict=True):
-        print(f"{fs.shape}, {ts=}, {slice_args=}")
-        assert torch.equal(fs, ref[slice(*slice_args)])
+def _is_ffmpeg4():
+    vers = spdl.io.utils.get_ffmpeg_versions()
+    print(vers)
+    return vers["libavutil"][0] < 57
 
 
+@pytest.mark.skipif(
+    _is_ffmpeg4(), reason="FFmpeg4 is known to return a different result."
+)
 def test_color_conversion_rgba(get_sample):
     """Providing pix_fmt="rgba" should produce (N,4,H,W) array."""
     # fmt: off
