eb6e2d810284d97ab3c6472ab321a4cc7ddb70c1
Moto Hira <moto@meta.com>
2025-03-28 14:09:36 -0700

    Test pixel parity against FFmpeg, and fix native YUV conversion (#582)
    Summary: Pull Request resolved: https://github.com/facebookresearch/spdl/pull/582
    
    Reviewed By: nateanl
    
    Differential Revision: D72059101

--- tests/spdl_unittest/fixture.py
+++ tests/spdl_unittest/fixture.py
@@ -7,19 +7,27 @@
 # pyre-strict
 
 import subprocess
+import sys
 from dataclasses import dataclass
 from pathlib import Path
 from tempfile import TemporaryDirectory
+
+import numpy as np
+from numpy.typing import DTypeLike, NDArray
 
 __all__ = [
     "get_sample",
     "get_samples",
     "SrcInfo",
+    "load_ref_video",
+    "load_ref_image",
 ]
 
 
 def _run_in_tmpdir(cmd: str, tmp_dir: Path) -> None:
-    print(f"Executing `{cmd}`")
+    print(f"\n{'-' * 40}", flush=True, file=sys.stderr)
+    print(f"- Executing `{cmd}`", flush=True, file=sys.stderr)
+    print(f"{'-' * 40}\n", flush=True, file=sys.stderr)
     subprocess.run(cmd, cwd=tmp_dir, shell=True, check=True, capture_output=False)
 
 
@@ -41,3 +49,55 @@ def get_samples(cmd: str) -> list[SrcInfo]:
 
     _run_in_tmpdir(cmd, tmp_path)
     return [SrcInfo(str(f), tmp_dir) for f in tmp_path.glob("**/*") if f.is_file()]
+
+
+def load_ref_data(
+    cmd: list[str],
+    shape: tuple[int, ...],
+    *,
+    dtype: DTypeLike = np.uint8,
+) -> NDArray[np.uint8]:
+    print(f"\n{'-' * 40}", flush=True, file=sys.stderr)
+    print(f"- Executing `{' '.join(cmd)}`", flush=True, file=sys.stderr)
+    print(f"{'-' * 40}\n", flush=True, file=sys.stderr)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=10**8)
+    buffer = process.stdout.read()  # pyre-ignore
+    return np.frombuffer(buffer, dtype).reshape(*shape)
+
+
+def _get_video_ref_cmd(path: str, filter_graph: str | None) -> list[str]:
+    # fmt: off
+    command = [
+        "ffmpeg",
+        "-hide_banner",
+        "-loglevel", "debug",
+        "-y",
+        "-i", path,
+        "-v", "verbose"
+    ]
+    # fmt: on
+    if filter_graph is not None:
+        command.extend(["-vf", filter_graph])
+    command.extend(["-f", "rawvideo", "pipe:"])
+    return command
+
+
+def load_ref_video(
+    path: str,
+    shape: tuple[int, ...],
+    *,
+    filter_graph: str | None = "format=pix_fmts=rgb24",
+    dtype: DTypeLike = np.uint8,
+) -> NDArray[np.uint8]:
+    cmd = _get_video_ref_cmd(path, filter_graph)
+    return load_ref_data(cmd, shape, dtype=dtype)
+
+
+def load_ref_image(
+    path: str,
+    shape: tuple[int, ...],
+    *,
+    filter_graph: str | None = "format=pix_fmts=rgb24",
+    dtype: DTypeLike = np.uint8,
+) -> NDArray[np.uint8]:
+    return load_ref_video(path, shape, dtype=dtype, filter_graph=filter_graph)
