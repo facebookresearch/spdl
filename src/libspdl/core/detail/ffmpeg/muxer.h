170acb41819c1725c65a0e0881f8c0db694a1147
moto <855818+mthrok@users.noreply.github.com>
2025-04-01 12:40:46 -0400

    Add Muxer implementation (#594)

---
+++ src/libspdl/core/detail/ffmpeg/muxer.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <libspdl/core/codec.h>
+#include <libspdl/core/types.h>
+
+#include "libspdl/core/detail/ffmpeg/encoder.h"
+#include "libspdl/core/detail/ffmpeg/wrappers.h"
+
+#include <optional>
+#include <string>
+
+namespace spdl::core::detail {
+
+class MuxerImpl {
+  AVFormatOutputContextPtr fmt_ctx;
+
+ public:
+  MuxerImpl(const std::string& uri, const std::optional<std::string>& format);
+
+  std::unique_ptr<VideoEncoderImpl> add_video_encode_stream(
+      const VideoEncodeConfig& codec_config,
+      const std::optional<std::string>& encoder,
+      const std::optional<OptionDict>& encoder_config);
+
+  void add_video_remux_stream(const VideoCodec& codec);
+
+  void open(const std::optional<OptionDict>& muxer_config);
+
+  void
+  write(int i, const std::vector<AVPacket*>& packets, AVRational time_base);
+  void flush();
+  void close();
+};
+
+} // namespace spdl::core::detail
---
+++ src/libspdl/core/muxer.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <libspdl/core/muxer.h>
+
+#include "libspdl/core/detail/ffmpeg/muxer.h"
+
+namespace spdl::core {
+
+Muxer::Muxer(const std::string& uri, const std::optional<std::string>& muxer)
+    : pImpl(new detail::MuxerImpl(uri, muxer)) {}
+
+Muxer::~Muxer() {
+  delete pImpl;
+}
+
+VideoEncoderPtr Muxer::add_video_encode_stream(
+    const VideoEncodeConfig& codec_config,
+    const std::optional<std::string>& encoder,
+    const std::optional<OptionDict>& encoder_config) {
+  auto p =
+      pImpl->add_video_encode_stream(codec_config, encoder, encoder_config);
+  return std::make_unique<VideoEncoder>(p.release());
+}
+
+void Muxer::add_video_remux_stream(const VideoCodec& codec) {
+  pImpl->add_video_remux_stream(codec);
+}
+
+void Muxer::open(const std::optional<OptionDict>& muxer_config) {
+  pImpl->open(muxer_config);
+}
+
+template <MediaType media_type>
+void Muxer::write(int i, DemuxedPackets<media_type>& packets) {
+  pImpl->write(i, packets.get_packets(), packets.codec.get_time_base());
+}
+
+template void Muxer::write<MediaType::Video>(
+    int,
+    DemuxedPackets<MediaType::Video>&);
+
+void Muxer::flush() {
+  pImpl->flush();
+}
+
+void Muxer::close() {
+  pImpl->close();
+}
+
+} // namespace spdl::core
