921b9433a8efca9c711546e2468a14f5486dfc69
moto <855818+mthrok@users.noreply.github.com>
2025-03-31 22:21:57 -0400

    Move AVFORMAT_CONST to compat.h (#590)

--- src/libspdl/core/detail/ffmpeg/compat.h
+++ src/libspdl/core/detail/ffmpeg/compat.h
@@ -9,6 +9,7 @@
 #pragma once
 
 extern "C" {
+#include <libavformat/version.h>
 #include <libavutil/version.h>
 }
 
@@ -19,4 +20,13 @@ extern "C" {
 #else
 #define GET_NUM_CHANNELS(x) x->channels
 #define GET_LAYOUT(x) x->channel_layout
+#endif
+
+// https://github.com/FFmpeg/FFmpeg/blob/4e6debe1df7d53f3f59b37449b82265d5c08a172/doc/APIchanges#L252-L260
+// Starting from libavformat 59 (ffmpeg 5),
+// AVInputFormat is const and related functions expect constant.
+#if LIBAVFORMAT_VERSION_MAJOR >= 59
+#define AVFORMAT_CONST const
+#else
+#define AVFORMAT_CONST
 #endif
