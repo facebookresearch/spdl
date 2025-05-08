dc8ad6d6ba6928e470f5c5c6347fa1da9085ec7b
moto <855818+mthrok@users.noreply.github.com>
2025-05-08 17:39:53 -0400

    Fix Typo

--- tests/spdl_unittest/dataloader/pipeline_test.py
+++ tests/spdl_unittest/dataloader/pipeline_test.py
@@ -833,7 +833,7 @@ def test_pipeline_resume():
 
 
 def test_pipeline_infinite_loop():
-    """AsyncPipeline can execute inifinite iterable"""
+    """AsyncPipeline can execute infinite iterable"""
 
     def src(i=-1):
         while True:
