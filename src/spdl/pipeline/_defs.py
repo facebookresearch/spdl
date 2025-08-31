9cc239cde2e6f9bba348931c5907c280e94a3fbf
moto <855818+mthrok@users.noreply.github.com>
2025-08-30 20:09:11 -0400

    Move pipe logic to defs

--- src/spdl/pipeline/_defs.py
+++ src/spdl/pipeline/_defs.py
@@ -5,16 +5,16 @@
 # LICENSE file in the root directory of this source tree.
 
 import inspect
-from collections.abc import AsyncIterable, Iterable
+from collections.abc import AsyncIterable, Callable, Iterable
 from concurrent.futures import Executor
 from dataclasses import dataclass
 from enum import IntEnum
-from typing import Generic, TypeVar
+from functools import partial
+from typing import Any, Generic, Protocol, runtime_checkable, TypeAlias, TypeVar
 
 from ._convert import Callables
 
 # pyre-strict
-
 
 T = TypeVar("T")
 U = TypeVar("U")
@@ -22,9 +22,11 @@ U = TypeVar("U")
 __all__ = [
     "_SourceConfig",
     "_PipeType",
-    "_PipeArgs",
     "_PipeConfig",
     "_SinkConfig",
+    "Aggregate",
+    "Disaggregate",
+    "Pipe",
 ]
 
 
@@ -106,3 +108,91 @@ class _SinkConfig(Generic[T]):
             raise ValueError(
                 f"`buffer_size` must be greater than 0. Found: {self.buffer_size}"
             )
+
+
+##############################################################################
+# Specialization for ease of use for users.
+##############################################################################
+def _get_op_name(op: Callable) -> str:
+    if isinstance(op, partial):
+        return _get_op_name(op.func)
+    return getattr(op, "__name__", op.__class__.__name__)
+
+
+@runtime_checkable
+class SupportsGetItem(Protocol[T, U]):
+    def __getitem__(self, key: T) -> U: ...
+
+
+_TPipeInputs: TypeAlias = Callables[T, U] | SupportsGetItem[T, U]
+
+
+def Pipe(
+    op: _TPipeInputs[T, U],
+    /,
+    *,
+    concurrency: int = 1,
+    executor: Executor | None = None,
+    name: str | None = None,
+    output_order: str = "completion",
+) -> _PipeConfig[T, U]:
+    if output_order not in ["completion", "input"]:
+        raise ValueError(
+            '`output_order` must be either "completion" or "input". '
+            f"Found: {output_order}"
+        )
+
+    type_ = _PipeType.Pipe if output_order == "completion" else _PipeType.OrderedPipe
+
+    if isinstance(op, SupportsGetItem):
+        # Note, if op is list/dict/tuple with a lot of elements, then
+        # debug print on `_ProcessConfig` might produce extremely long string.
+        # So it is important to extract the __getitem__ before it is passed to
+        # `_ProcessConfig`.
+        op = op.__getitem__
+
+        # We could do the same for callable (__call__)
+        # but usually callable class name contains readable information, so
+        # we don't do that here. (it happens in to_async helper function)
+
+    return _PipeConfig(
+        type_=type_,
+        name=name or _get_op_name(op),
+        args=_PipeArgs(
+            op=op,
+            executor=executor,
+            concurrency=concurrency,
+        ),
+    )
+
+
+def Aggregate(num_items: int, /, *, drop_last: bool = False) -> _PipeConfig[Any, Any]:
+    # To avoid circular deps
+    from ._components._pipe import _Aggregate
+
+    name = (
+        f"aggregate({num_items}, {drop_last=})"
+        if drop_last
+        else f"aggregate({num_items})"
+    )
+    return _PipeConfig(
+        _PipeType.Aggregate,
+        name=name,
+        args=_PipeArgs(
+            op=_Aggregate(num_items, drop_last),
+            op_requires_eof=True,
+        ),
+    )
+
+
+def Disaggregate() -> _PipeConfig[Any, Any]:
+    # To avoid circular deps
+    from ._components._pipe import _disaggregate
+
+    return _PipeConfig(
+        _PipeType.Disaggregate,
+        name="disaggregate",
+        args=_PipeArgs(
+            op=_disaggregate,  # pyre-ignore: [6]
+        ),
+    )
