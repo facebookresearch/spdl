70333cd93030f44e0a596561acdf4827d2aad135
moto <moto@fb.com>
2025-10-19 19:18:54 -0700

    Fix BUCK
    Differential Revision: D84997215

--- src/spdl/pipeline/_components/_hook.py
+++ src/spdl/pipeline/_components/_hook.py
@@ -15,7 +15,8 @@ from contextlib import asynccontextmanager, AsyncExitStack
 from dataclasses import dataclass
 from typing import AsyncContextManager, TypeVar
 
-from .._common._misc import create_task
+from spdl.pipeline._common._misc import create_task
+
 from ._common import _periodic_dispatch, _StatsCounter, _time_str
 
 __all__ = [
