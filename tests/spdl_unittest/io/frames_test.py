b5921462b61b032f7427f9bccf13556a7af326d0
Victor Bourgin <vbourgin@meta.com>
2025-10-24 00:12:43 -0700

    Migrate frames_test.py from pytest to unittest
    Differential Revision: D85404048

--- tests/spdl_unittest/io/frames_test.py
+++ tests/spdl_unittest/io/frames_test.py
@@ -6,39 +6,43 @@
 
 # pyre-unsafe
 
+import unittest
+
 import spdl.io
 
 from ..fixture import FFMPEG_CLI, get_sample
 
 
-def test_image_frame_metadata():
-    """Smoke test for image frame metadata.
-    Ideally, we should use images with EXIF data, but ffmpeg
-    does not seem to support exif, and I don't want to check-in
-    assets data, so just smoke test.
-    """
-    cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 1 sample.jpg"
-    sample = get_sample(cmd)
+class TestFrames(unittest.TestCase):
+    def test_image_frame_metadata(self) -> None:
+        """Smoke test for image frame metadata.
+        Ideally, we should use images with EXIF data, but ffmpeg
+        does not seem to support exif, and I don't want to check-in
+        assets data, so just smoke test.
+        """
+        cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 1 sample.jpg"
+        sample = get_sample(cmd)
 
-    packets = spdl.io.demux_image(sample.path)
-    frames = spdl.io.decode_packets(packets)
+        packets = spdl.io.demux_image(sample.path)
+        frames = spdl.io.decode_packets(packets)
 
-    assert frames.metadata == {}
+        self.assertEqual(frames.metadata, {})
 
+    def test_sample_frame_timestamps(self) -> None:
+        cmd = (
+            f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 10 sample.mp4"
+        )
+        sample = get_sample(cmd)
 
-def test_sample_frame_timestamps():
-    cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 10 sample.mp4"
-    sample = get_sample(cmd)
+        packets = spdl.io.demux_video(sample.path)
+        frames = spdl.io.decode_packets(packets)
+        print(frames.get_pts())
+        print(frames.get_timestamps())
+        print(frames.time_base)
 
-    packets = spdl.io.demux_video(sample.path)
-    frames = spdl.io.decode_packets(packets)
-    print(frames.get_pts())
-    print(frames.get_timestamps())
-    print(frames.time_base)
+        expected_pts = [0, 512, 1024, 1536, 2048, 2560, 3072, 3584, 4096, 4608]
+        num, den = frames.time_base
 
-    expected_pts = [0, 512, 1024, 1536, 2048, 2560, 3072, 3584, 4096, 4608]
-    num, den = frames.time_base
-
-    assert den / num == 12800
-    assert frames.get_pts() == expected_pts
-    assert frames.get_timestamps() == [t * num / den for t in expected_pts]
+        self.assertEqual(den / num, 12800)
+        self.assertEqual(frames.get_pts(), expected_pts)
+        self.assertEqual(frames.get_timestamps(), [t * num / den for t in expected_pts])
