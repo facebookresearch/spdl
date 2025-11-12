40ab436fe1076fb13396f766ea99e29348ddfbbd
Victor Bourgin <vbourgin@meta.com>
2025-11-11 16:11:32 -0800

    Migrate nvjpeg_decode_test.py from pytest to unittest
    Differential Revision: D85995153

--- tests/spdl_unittest/cuda/nvjpeg_decode_test.py
+++ tests/spdl_unittest/cuda/nvjpeg_decode_test.py
@@ -4,9 +4,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import unittest
 from random import randbytes
 
-import pytest
 import spdl.io
 import spdl.io.utils
 import torch
@@ -17,139 +17,134 @@ DEFAULT_CUDA = 0
 
 
 if not spdl.io.utils.built_with_nvjpeg():
-    pytest.skip(  # pyre-ignore: [29]
-        "SPDL is not compiled with NVJPEG support", allow_module_level=True
-    )
+    raise unittest.SkipTest("SPDL is not compiled with NVJPEG support")
 
 
-def test_decode_pix_fmt():
-    """"""
-    cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 1 sample.jpg"
-    sample = get_sample(cmd)
+class TestNvjpegDecode(unittest.TestCase):
+    def test_decode_pix_fmt(self) -> None:
+        """"""
+        cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 1 sample.jpg"
+        sample = get_sample(cmd)
 
-    def _test(data, pix_fmt):
-        buffer = spdl.io.decode_image_nvjpeg(
-            data,
-            device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
-            pix_fmt=pix_fmt,
-        )
-        tensor = spdl.io.to_torch(buffer)
-        assert tensor.dtype == torch.uint8
-        assert tensor.shape == torch.Size([3, 240, 320])
-        assert tensor.device == torch.device("cuda", DEFAULT_CUDA)
-        assert not torch.equal(tensor[0], tensor[1])
-        assert not torch.equal(tensor[1], tensor[2])
-        assert not torch.equal(tensor[2], tensor[0])
-        return tensor
+        def _test(data, pix_fmt):
+            buffer = spdl.io.decode_image_nvjpeg(
+                data,
+                device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
+                pix_fmt=pix_fmt,
+            )
+            tensor = spdl.io.to_torch(buffer)
+            self.assertEqual(tensor.dtype, torch.uint8)
+            self.assertEqual(tensor.shape, torch.Size([3, 240, 320]))
+            self.assertEqual(tensor.device, torch.device("cuda", DEFAULT_CUDA))
+            self.assertFalse(torch.equal(tensor[0], tensor[1]))
+            self.assertFalse(torch.equal(tensor[1], tensor[2]))
+            self.assertFalse(torch.equal(tensor[2], tensor[0]))
+            return tensor
 
-    rgb_tensor = _test(sample.path, "rgb")
-    bgr_tensor = _test(sample.path, "bgr")
+        rgb_tensor = _test(sample.path, "rgb")
+        bgr_tensor = _test(sample.path, "bgr")
 
-    assert torch.equal(rgb_tensor[0], bgr_tensor[2])
-    assert torch.equal(rgb_tensor[1], bgr_tensor[1])
-    assert torch.equal(rgb_tensor[2], bgr_tensor[0])
+        self.assertTrue(torch.equal(rgb_tensor[0], bgr_tensor[2]))
+        self.assertTrue(torch.equal(rgb_tensor[1], bgr_tensor[1]))
+        self.assertTrue(torch.equal(rgb_tensor[2], bgr_tensor[0]))
 
+    def test_decode_rubbish(self) -> None:
+        """When decoding fails, it should raise an error instead of segfault then,
+        subsequent valid decodings should succeed"""
 
-def test_decode_rubbish():
-    """When decoding fails, it should raise an error instead of segfault then,
-    subsequent valid decodings should succeed"""
+        cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 10 sample_%10d.jpg"
+        srcs = get_samples(cmd)
 
-    cmd = (
-        f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 10 sample_%10d.jpg"
-    )
-    srcs = get_samples(cmd)
+        for _ in range(10):
+            rubbish = randbytes(2096)
+            with self.assertRaises(RuntimeError):
+                spdl.io.decode_image_nvjpeg(
+                    rubbish,
+                    device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
+                )
 
-    for _ in range(10):
-        rubbish = randbytes(2096)
-        with pytest.raises(RuntimeError):
-            spdl.io.decode_image_nvjpeg(
-                rubbish,
+        for src in srcs:
+            buffer = spdl.io.decode_image_nvjpeg(
+                src.path,
                 device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
             )
 
-    for src in srcs:
+            tensor = spdl.io.to_torch(buffer)
+            self.assertEqual(tensor.dtype, torch.uint8)
+            self.assertEqual(tensor.shape, torch.Size([3, 240, 320]))
+            self.assertEqual(tensor.device, torch.device("cuda", DEFAULT_CUDA))
+            self.assertFalse(torch.equal(tensor[0], tensor[1]))
+            self.assertFalse(torch.equal(tensor[1], tensor[2]))
+            self.assertFalse(torch.equal(tensor[2], tensor[0]))
+
+    def test_decode_resize(self) -> None:
+        cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 1 sample.jpg"
+        sample = get_sample(cmd)
+
         buffer = spdl.io.decode_image_nvjpeg(
-            src.path,
+            sample.path,
             device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
+            scale_width=160,
+            scale_height=120,
         )
-
         tensor = spdl.io.to_torch(buffer)
-        assert tensor.dtype == torch.uint8
-        assert tensor.shape == torch.Size([3, 240, 320])
-        assert tensor.device == torch.device("cuda", DEFAULT_CUDA)
-        assert not torch.equal(tensor[0], tensor[1])
-        assert not torch.equal(tensor[1], tensor[2])
-        assert not torch.equal(tensor[2], tensor[0])
-
-
-def test_decode_resize():
-    cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 1 sample.jpg"
-    sample = get_sample(cmd)
-
-    buffer = spdl.io.decode_image_nvjpeg(
-        sample.path,
-        device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
-        scale_width=160,
-        scale_height=120,
-    )
-    tensor = spdl.io.to_torch(buffer)
-    assert tensor.dtype == torch.uint8
-    assert tensor.shape == torch.Size([3, 120, 160])
-    assert tensor.device == torch.device("cuda", DEFAULT_CUDA)
-    assert not torch.equal(tensor[0], tensor[1])
-    assert not torch.equal(tensor[1], tensor[2])
-    assert not torch.equal(tensor[2], tensor[0])
+        self.assertEqual(tensor.dtype, torch.uint8)
+        self.assertEqual(tensor.shape, torch.Size([3, 120, 160]))
+        self.assertEqual(tensor.device, torch.device("cuda", DEFAULT_CUDA))
+        self.assertFalse(torch.equal(tensor[0], tensor[1]))
+        self.assertFalse(torch.equal(tensor[1], tensor[2]))
+        self.assertFalse(torch.equal(tensor[2], tensor[0]))
 
 
 def _is_all_zero(arr):
     return all(int(v) == 0 for v in arr)
 
 
-def test_decode_zero_clear():
-    cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 1 sample.jpg"
-    sample = get_sample(cmd)
+class TestNvjpegDecodeZeroClear(unittest.TestCase):
+    def test_decode_zero_clear(self) -> None:
+        cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 1 sample.jpg"
+        sample = get_sample(cmd)
 
-    with open(sample.path, "rb") as f:
-        data = f.read()
+        with open(sample.path, "rb") as f:
+            data = f.read()
 
-    assert not _is_all_zero(data)
+        self.assertFalse(_is_all_zero(data))
 
-    buffer = spdl.io.decode_image_nvjpeg(
-        data,
-        device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
-        scale_width=160,
-        scale_height=120,
-        _zero_clear=True,
-    )
-    tensor = spdl.io.to_torch(buffer)
-    assert tensor.dtype == torch.uint8
-    assert tensor.shape == torch.Size([3, 120, 160])
-    assert tensor.device == torch.device("cuda", DEFAULT_CUDA)
+        buffer = spdl.io.decode_image_nvjpeg(
+            data,
+            device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
+            scale_width=160,
+            scale_height=120,
+            _zero_clear=True,
+        )
+        tensor = spdl.io.to_torch(buffer)
+        self.assertEqual(tensor.dtype, torch.uint8)
+        self.assertEqual(tensor.shape, torch.Size([3, 120, 160]))
+        self.assertEqual(tensor.device, torch.device("cuda", DEFAULT_CUDA))
 
-    assert _is_all_zero(data)
+        self.assertTrue(_is_all_zero(data))
 
+    def test_batch_decode_zero_clear(self) -> None:
+        cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 100 sample_%03d.jpg"
+        srcs = get_samples(cmd)
 
-def test_batch_decode_zero_clear():
-    cmd = f"{FFMPEG_CLI} -hide_banner -y -f lavfi -i testsrc -frames:v 100 sample_%03d.jpg"
-    srcs = get_samples(cmd)
+        dataset = []
+        for src in srcs:
+            with open(src.path, "rb") as f:
+                dataset.append(f.read())
 
-    dataset = []
-    for src in srcs:
-        with open(src.path, "rb") as f:
-            dataset.append(f.read())
+        self.assertTrue(all(not _is_all_zero(data) for data in dataset))
 
-    assert all(not _is_all_zero(data) for data in dataset)
+        buffer = spdl.io.load_image_batch_nvjpeg(
+            dataset,
+            device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
+            width=160,
+            height=120,
+            _zero_clear=True,
+        )
+        tensor = spdl.io.to_torch(buffer)
+        self.assertEqual(tensor.dtype, torch.uint8)
+        self.assertEqual(tensor.shape, torch.Size([100, 3, 120, 160]))
+        self.assertEqual(tensor.device, torch.device("cuda", DEFAULT_CUDA))
 
-    buffer = spdl.io.load_image_batch_nvjpeg(
-        dataset,
-        device_config=spdl.io.cuda_config(device_index=DEFAULT_CUDA),
-        width=160,
-        height=120,
-        _zero_clear=True,
-    )
-    tensor = spdl.io.to_torch(buffer)
-    assert tensor.dtype == torch.uint8
-    assert tensor.shape == torch.Size([100, 3, 120, 160])
-    assert tensor.device == torch.device("cuda", DEFAULT_CUDA)
-
-    assert all(_is_all_zero(data) for data in dataset)
+        self.assertTrue(all(_is_all_zero(data) for data in dataset))
