5c66ff28a703299987404f2b586b01b464b75131
moto <855818+mthrok@users.noreply.github.com>
2025-11-19 18:28:59 -0500

    Add note

---
+++ src/spdl/pipeline/_common/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+##############################################################################
+# NOTE:
+# We do not encapsulate this `_common` submodule like we do in other
+# submodules like `spdl.pipeline._components` because this submodule is
+# just a collection of unrelated functions used by other modules.
+# Each functions and source files are self-contained, and there should be
+# not much relationship between them.
+# So please do not add re-expose mechanism here.
+##############################################################################
