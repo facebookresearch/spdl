5e7afb18ddaee883b5a2a4c9feeffe985b3605b3
moto <855818+mthrok@users.noreply.github.com>
2025-11-21 14:43:26 -0500

    Apply lint to Python files in .github dir (#1161)

--- .github/scripts/check_deps.py
+++ .github/scripts/check_deps.py
@@ -16,61 +16,63 @@ def check_package_meta():
 def _get_imported_3rd_party_modules():
     ret = set()
     for name, mod in sys.modules.items():
-        if hasattr(mod, '__path__'):
-            if any('site-packages' in p for p in mod.__path__):
+        if hasattr(mod, "__path__"):
+            if any("site-packages" in p for p in mod.__path__):
                 ret.add(name)
     return ret
 
 
 def check_imported_modules():
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     # Pre-condition
-    #--------------------------------------------------------------------------
-    print('Checking the target modules are not yet imported.')
-    base_mods  = _get_imported_3rd_party_modules()
-    if violation := base_mods & set(('torch', 'numpy', 'jax', 'spdl')):
+    # --------------------------------------------------------------------------
+    print("Checking the target modules are not yet imported.")
+    base_mods = _get_imported_3rd_party_modules()
+    if violation := base_mods & set(("torch", "numpy", "jax", "spdl")):
         raise RuntimeError(
-            "The following modules must not be imported before testing: "
-            f"{violation}")
+            "The following modules must not be imported before testing: " f"{violation}"
+        )
 
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     # Import must success without third party packages installed
-    #--------------------------------------------------------------------------
-    print('Testing the spdl_core module import')
+    # --------------------------------------------------------------------------
+    print("Testing the spdl_core module import")
 
     import spdl.pipeline
 
     mods = _get_imported_3rd_party_modules()
-    assert 'spdl.pipeline' in mods
-    mods = {m for m in mods - base_mods if not m.startswith('spdl')}
+    assert "spdl.pipeline" in mods
+    mods = {m for m in mods - base_mods if not m.startswith("spdl")}
     print(f"Modules imported with spdl.pipeline: {mods}")
-    if violation := mods & set(('torch', 'numpy', 'jax')):
+    if violation := mods & set(("torch", "numpy", "jax")):
         raise RuntimeError(
             "`import spdl.pipeline` must not import third-party libraries. "
-            f"Found: {violation}")
+            f"Found: {violation}"
+        )
 
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     # Import (Same as above but NumPy is allowed
-    #--------------------------------------------------------------------------
-    print('Testing the spdl_io module import')
+    # --------------------------------------------------------------------------
+    print("Testing the spdl_io module import")
 
     import spdl.io
 
     mods = _get_imported_3rd_party_modules()
-    assert 'spdl.io' in mods
-    mods = {m for m in mods - base_mods if not m.startswith('spdl')}
+    assert "spdl.io" in mods
+    mods = {m for m in mods - base_mods if not m.startswith("spdl")}
     print(f"Modules imported with spdl.io: {mods}")
-    if violation := mods & set(('torch', 'jax')):
+    if violation := mods & set(("torch", "jax")):
         raise RuntimeError(
             "`import spdl.io` must not import third-party libraries except NumPy."
-            f"Found: {violation}")
+            f"Found: {violation}"
+        )
 
 
 def main():
     check_package_meta()
     check_imported_modules()
-    print('OK')
+    print("OK")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
