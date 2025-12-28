6bc526875c237cac33e8e78c9b2d8ba839b185b7
moto <855818+mthrok@users.noreply.github.com>
2025-12-27 21:03:05 -0500

    [libspdl] Remove the original NV12 color conversion function
    With the changes introduced in
    https://github.com/facebookresearch/spdl/pull/1271 ,
    NVDEC decoder only returns batched frames, and corresponding
    NV12 conversion function was added in
    https://github.com/facebookresearch/spdl/pull/1269 .
    
    These changes make the original NV12 color conversion function
    (the one takes a list of single frames) obsolete and disconnected
    from the rest of the APIs.
    
    So this commit removes the funciton.

--- src/libspdl/cuda/detail/color_conversion.h
+++ src/libspdl/cuda/detail/color_conversion.h
@@ -15,29 +15,8 @@ using CUstream = CUstream_st*;
 
 namespace spdl::cuda::detail {
 
-// Non-batched version (single frame)
-void nv12_to_planar_rgb(
-    CUstream stream,
-    uint8_t* src,
-    int src_pitch,
-    uint8_t* dst,
-    int dst_pitch,
-    int width,
-    int height,
-    int matrix_coefficients);
-
-void nv12_to_planar_bgr(
-    CUstream stream,
-    uint8_t* src,
-    int src_pitch,
-    uint8_t* dst,
-    int dst_pitch,
-    int width,
-    int height,
-    int matrix_coefficients);
-
 // Batched version (multiple frames in single kernel launch)
-void nv12_to_planar_rgb_batched(
+void nv12_to_planar_rgb(
     CUstream stream,
     uint8_t* src,
     int src_pitch,
@@ -48,7 +27,7 @@ void nv12_to_planar_rgb_batched(
     int num_frames,
     int matrix_coefficients);
 
-void nv12_to_planar_bgr_batched(
+void nv12_to_planar_bgr(
     CUstream stream,
     uint8_t* src,
     int src_pitch,
--- src/spdl/io/lib/cuda/color_conversion.cpp
+++ src/spdl/io/lib/cuda/color_conversion.cpp
@@ -16,7 +16,6 @@
 
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/unique_ptr.h>
-#include <nanobind/stl/vector.h>
 
 namespace nb = nanobind;
 
@@ -25,43 +24,11 @@ void register_color_conversion(nb::module_& m) {
   m.def(
       "nv12_to_planar_rgb",
 #ifndef SPDL_USE_CUDA
-      [](const std::vector<CUDABuffer>&, const CUDAConfig&, int, bool)
-          -> CUDABufferPtr {
-        throw std::runtime_error("SPDL is not built with CUDA support.");
-      },
-#else
-      &nv12_to_planar_rgb,
-#endif
-      nb::call_guard<nb::gil_scoped_release>(),
-      nb::arg("buffers"),
-      nb::kw_only(),
-      nb::arg("device_config"),
-      nb::arg("matrix_coeff") = 1,
-      nb::arg("sync") = true);
-  m.def(
-      "nv12_to_planar_bgr",
-#ifndef SPDL_USE_CUDA
-      [](const std::vector<CUDABuffer>&, const CUDAConfig&, int, bool)
-          -> CUDABufferPtr {
-        throw std::runtime_error("SPDL is not built with CUDA support.");
-      },
-#else
-      &nv12_to_planar_bgr,
-#endif
-      nb::call_guard<nb::gil_scoped_release>(),
-      nb::arg("buffers"),
-      nb::kw_only(),
-      nb::arg("device_config"),
-      nb::arg("matrix_coeff") = 1,
-      nb::arg("sync") = true);
-  m.def(
-      "nv12_to_planar_rgb",
-#ifndef SPDL_USE_CUDA
       [](const CUDABuffer&, const CUDAConfig&, int, bool) -> CUDABufferPtr {
         throw std::runtime_error("SPDL is not built with CUDA support.");
       },
 #else
-      &nv12_to_planar_rgb_batched,
+      &nv12_to_planar_rgb,
 #endif
       R"(Convert batched NV12 frames to planar RGB.
 
@@ -87,7 +54,7 @@ Returns:
         throw std::runtime_error("SPDL is not built with CUDA support.");
       },
 #else
-      &nv12_to_planar_bgr_batched,
+      &nv12_to_planar_bgr,
 #endif
       R"(Convert batched NV12 frames to planar BGR.
 
