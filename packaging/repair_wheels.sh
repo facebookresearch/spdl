a2c11c5ea8c031fd88e89168a4dde78b968941bc
moto <855818+mthrok@users.noreply.github.com>
2026-08-25 12:17:31 -0700

    Add FFmpeg9 to LGPL job

--- packaging/repair_wheels.sh
+++ packaging/repair_wheels.sh
@@ -21,26 +21,31 @@ for whl in "${input_dir}"/*.whl; do
                    --exclude libavcodec.so.60   \
                    --exclude libavcodec.so.61   \
                    --exclude libavcodec.so.62   \
+                   --exclude libavcodec.so.63   \
                    --exclude libavfilter.so.7   \
                    --exclude libavfilter.so.8   \
                    --exclude libavfilter.so.9   \
                    --exclude libavfilter.so.10  \
                    --exclude libavfilter.so.11  \
+                   --exclude libavfilter.so.12  \
                    --exclude libavdevice.so.58  \
                    --exclude libavdevice.so.59  \
                    --exclude libavdevice.so.60  \
                    --exclude libavdevice.so.61  \
                    --exclude libavdevice.so.62  \
+                   --exclude libavdevice.so.63  \
                    --exclude libavformat.so.58  \
                    --exclude libavformat.so.59  \
                    --exclude libavformat.so.60  \
                    --exclude libavformat.so.61  \
                    --exclude libavformat.so.62  \
+                   --exclude libavformat.so.63  \
                    --exclude libavutil.so.56    \
                    --exclude libavutil.so.57    \
                    --exclude libavutil.so.58    \
                    --exclude libavutil.so.59    \
                    --exclude libavutil.so.60    \
+                   --exclude libavutil.so.61    \
                    --exclude libcuda.so.1       \
                    --exclude libnvcuvid.so      \
                    --exclude libcudart.so.11.0  \
